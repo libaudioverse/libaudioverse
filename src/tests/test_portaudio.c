96f5c8b981be60d48e1dcee1587d82411c6adc9c
Austin Hicks <camlorn38@gmail.com>
2014-04-26 00:53:39 -0400

    Tests now start with test_; started test_portaudio.

---
+++ src/tests/test_portaudio.c
@@ -0,0 +1,5 @@
+/**This is a test of portaudio which serves two purposes: offering a suitable debug case and demonstrating proper usage of the library.
+This test does not involve anything else.*/
+
+void main() {
+}
