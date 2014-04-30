7fb731c663029e9d2cb671b9255b477c116aecfe
Austin Hicks <camlorn38@gmail.com>
2014-04-30 17:42:03 -0400

    Added a function to free a node.

--- src/libaudioverse/nodes.c
+++ src/libaudioverse/nodes.c
@@ -1,5 +1,10 @@
+/**Handles functionality common to all nodes: linking, allocating, and freeing.*/
 #include <stdlib.h>
 #incllude <libaudioverse/libaudioverse.h>
+
+void freeNode(LavNode *node) {
+	free(node);
+}
 
 LavNode *makeNode(unsigned int size, unsigned int numInputs, unsigned int numOutputs, enum  Lav_NODETYPE type) {
 	LavNode *retval = calloc(0, size);
