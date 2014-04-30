38eb7f431f183c260945b7170b7f740ff1734a4c
Austin Hicks <camlorn38@gmail.com>
2014-04-30 17:13:30 -0400

    The node initialization function now takes a size, so that it can be used by functions of derived node types.

--- src/libaudioverse/nodes.c
+++ src/libaudioverse/nodes.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #incllude <libaudioverse/libaudioverse.h>
 
-LavNode *makeNode(unsigned int numInputs, unsigned int numOutputs, enum  Lav_NODETYPE type) {
-	LavNode *retval = calloc(0, sizeof(LavNode));
+LavNode *makeNode(unsigned int size, unsigned int numInputs, unsigned int numOutputs, enum  Lav_NODETYPE type) {
+	LavNode *retval = calloc(0, size);
 	retval->num_inputs = numInputs;
 	retval->num_outputs = numOutputs;
 	retval->type = type;
