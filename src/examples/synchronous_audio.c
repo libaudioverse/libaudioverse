76cff3b8e19d51bf89c05387826477986a2547aa
Austin Hicks <camlorn38@gmail.com>
2014-05-04 21:37:35 -0400

    Created file for synchronous audio example.

---
+++ src/examples/synchronous_audio.c
@@ -0,0 +1,4 @@
+/**This example demonstrates how to perform synchronous audio using Portaudio and Libaudioverse, and serves as an example for how to get audio out of the library directly.*/
+
+void main() {
+}
