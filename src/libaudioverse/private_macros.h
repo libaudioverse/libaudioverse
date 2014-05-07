93f348ed5594580eb52a6ecbd6e4bb80f235d0ce
Austin Hicks <camlorn38@gmail.com>
2014-05-06 23:15:15 -0400

    Remove unneeded file.

--- src/libaudioverse/private_macros.h
+++
@@ -1,7 +0,0 @@
-/**Private macro definitions.*/
-
-#define ERROR_IF_TRUE(expression, error) do {\
-if(expression) return error;\
-} while(0)
-
-#define CHECK_NOT_NULL(ptr) ERROR_IF_TRUE(ptr == NULL, Lav_ERROR_NULL_POINTER)
