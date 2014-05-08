c9cff09c35f041a18e0106470f2de487817bd4c0
Austin Hicks <camlorn38@gmail.com>
2014-05-08 16:22:34 -0400

    Macros to abstract returning from a function holding locks.

--- include/libaudioverse/private_macros.h
+++ include/libaudioverse/private_macros.h
@@ -8,3 +8,15 @@ if(expression) return error;\
 } while(0)
 
 #define CHECK_NOT_NULL(ptr) ERROR_IF_TRUE(ptr == NULL, Lav_ERROR_NULL_POINTER)
+
+/**The following three macros abstract returning error codes, and make the cleanup logic for locks manageable.
+They exist because goto is a bad thing for clarity, and because they can.*/
+
+#define WILL_RETURN(type) type return_value
+
+#define RETURN(value) do {\
+return_value = value;\
+goto Do_return_and_cleanup;\
+} while(0)
+
+#define BEGIN_RETURN_BLOCK do_return_and_cleanup:
