cf14fea2a7f696d8964a70e4ff9c774861b049b6
Austin Hicks <camlorn38@gmail.com>
2014-05-10 22:09:09 -0400

    Finished and refactored preliminary threadsafe ringbuffer

--- src/libaudioverse/threading/crossthread_ringbuffer.c
+++ src/libaudioverse/threading/crossthread_ringbuffer.c
@@ -4,9 +4,10 @@ Todo: replace this with a lock-free implementation assuming that it becomes a bo
 */
 #include <libaudioverse/private_all.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
-	unsigned int reader_position, writer_position, element_size, length;
+	unsigned int read_position, write_position, element_size, length;
 	void* lock;
 	char* data;
 } LavCrossThreadRingBuffer;
@@ -16,17 +17,20 @@ Lav_PUBLIC_FUNCTION LavError createCrossThreadRingBuffer(unsigned int length, un
 	ERROR_IF_TRUE(length == 0 || elementSize == 0, Lav_ERROR_RANGE);
 	unsigned int data_size = length*elementSize;
 	char* data = calloc(1, data_size);
-	void* lock = createMutex();
+	void* lock;
+	createMutex(&lock);
 	CHECK_NOT_NULL(lock);
 	CHECK_NOT_NULL(data);
-	LavCrossThreadRingBuffer *retval = calloc(1, sizeof(LavCrossRingBuffer));
+	LavCrossThreadRingBuffer *retval = calloc(1, sizeof(LavCrossThreadRingBuffer));
 	CHECK_NOT_NULL(retval);
 	retval->lock = lock;
 	retval->data = data;
 	retval->read_position = retval->write_position = 0;
-	retval->element_size = element_size;
+	retval->element_size = elementSize;
 	retval->length = length;
 	*destination = retval;
+	BEGIN_CLEANUP_BLOCK
+	DO_ACTUAL_RETURN;
 }
 
 /**The following functions are high-performance.
@@ -40,9 +44,9 @@ Lav_PUBLIC_FUNCTION unsigned int crossThreadRingBufferGetAvailableWrites(LavCros
 	WILL_RETURN(unsigned int);
 	LOCK(buffer->lock);
 	//First, find out how far apart the two heads of the buffer are.
-	unsigned int used = (buffer->write_position-buffer->read_position)%buffer->length); //number of elements currently in use.
+	unsigned int used = (buffer->write_position-buffer->read_position)%buffer->length; //number of elements currently in use.
 	unsigned int available = buffer->length-used; //how many are left?
-	return(available);
+	RETURN(available);
 	STANDARD_CLEANUP_BLOCK(buffer->lock);
 }
 
@@ -55,45 +59,27 @@ Lav_PUBLIC_FUNCTION unsigned int crossThreadRingBufferGetAvailableReads(LavCross
 }
 
 //This function is, qutie honestly, really and truly as bad as it looks.
-#define RB_RETURN do { mutexUnlock(buffer->lock); return; ) while(0)
+#define RB_RETURN do { mutexUnlock(buffer->lock); return; }  while(0)
 Lav_PUBLIC_FUNCTION void crossThreadRingBufferGetItems(LavCrossThreadRingBuffer *buffer, unsigned int count, void* destination) {
 	memset(destination, 0, count*buffer->element_size);
-	LOCK(buffer->lock);
-	//First case: we don't have enough.
-	unsigned int available = crossThreadBufferGetAvailableReads(buffer);
-	if(available < count) {
-		crossThreadRingBufferGetItems(buffer, available, destination);
-		RB_RETURN;
+	mutexLock(buffer->lock);
+	unsigned int available = crossThreadRingBufferGetAvailableReads(buffer);
+	char* data = buffer->data;
+	for(unsigned int i = 0; i < available && count > 0; i++, count--) {
+		memcpy(destination, data+buffer->write_position*buffer->element_size, buffer->element_size);
+		buffer->read_position++;
+		buffer->read_position%=buffer->length;
 	}
-	//Next case: the read pointer is left of the write pointer:
-	if((buffer->write_position - buffer->read_position) > 0) {
-		//just a memcpy and return:
-		memcpy(destination, buffer->data+(buffer->read_position*buffer->element_size), buffer->element_size*count);
-		buffer->read_position += count;
-		RB_RETURN;
-	}
-
-	/*If we get here, we've got a complex algorithm. This is as bad as it looks.
-Briefly, the place between the read and write pointers is not in use, only the ends.  We need to copy off the right end first, and then copy off the left end if needed.*/
-	unsigned int right = buffer->length-buffer->read_position; //what's to the right?
-	unsigned int left = buffer->write_position; //what's to the left?
-	unsigned int needed = count;
-	if(right - needed > 0) { //Yay! It's really easy.
-		memcpy(destination, buffer->data+(right*buffer->element_size), needed*buffer->element_size);
-		buffer->position -+= needed; //we can move right without the mod.
-		RB_RETURN;
-	}
-
-	needed = count - right;
-	memcpy(destination, buffer->data+(buffer->element_size*buffer->read_position), right*buffer->element_size);
-	buffer->read_position+= right; //we're now at the right end of it.
-	//now, grab the beginning.
-	memcpy(destination+right*buffer->element_size, buffer->data, buffer->element_size*needed);
-	buffer->read_position = (buffer->read_position+needed)%buffer->length);
 	RB_RETURN;
 }
 
-Lav_PUBLIC_FUNCTION void crossThreadRingBufferWriteItems(LavCrossThreadRingBuffer *buffer, char* items, unsigned int count) {
-	mutexLock(buffer->mutex);
-	mutexUnlock(buffer->mutex);
+Lav_PUBLIC_FUNCTION void crossThreadRingBufferWriteItems(LavCrossThreadRingBuffer *buffer, unsigned int count, void* data) {
+	mutexLock(buffer->lock);
+	char* data_ptr = data;
+	unsigned int available = crossThreadRingBufferGetAvailableWrites(buffer);
+	for(unsigned int i = 0; i < available && count > 0; i++, count--) {
+		memcpy(buffer->data+buffer->write_position*buffer->element_size, data_ptr, buffer->element_size);
+		data_ptr += buffer->element_size;
+	}
+	RB_RETURN;
 }
