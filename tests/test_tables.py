5589f40d19ff7e5f00c36ec47829df68fe69485f
Austin Hicks <camlorn38@gmail.com>
2014-05-14 16:17:55 -0400

    Test for Lav_tableComputeSampleRange

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -19,12 +19,25 @@ def test_table_clear():
 		assert lav.Lav_tableGetSample(table, i, sample) == lav.Lav_ERROR_NONE
 		assert sample[0] < accuracy
 
-def test_table_get_sample():
-	original = [0, 1] #A triangle wave.
+#some values for a table.
+original = [0, 1] #A triangle wave.
+#correct values for the table used in these tests.
+halves= [0, 0.5, 1, 0.5]
+fourths = [0, 0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25]
+eighths = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 0.875, 0.75, 0.625, 0.5, 0.375, 0.25, 0.125]
+correct = [halves, fourths, eighths]
+
+def make_table():
+	global original
 	table = ffi.new("LavTable **")
 	assert lav.Lav_createTable(table) == lav.Lav_ERROR_NONE
 	table = table[0]
 	assert lav.Lav_tableSetSamples(table, len(original), original) == lav.Lav_ERROR_NONE
+	return table
+
+def test_table_get_sample():
+	global halves, fourths, eighths, correct, original, accuracy
+	table = make_table()
 	#test 1: reading exactly on integer boundaries should always produce the same value.  I like 500.
 	for i in xrange(500):
 		original_index = i%len(original)
@@ -32,10 +45,6 @@ def test_table_get_sample():
 		lav.Lav_tableGetSample(table, i, sample)
 		assert abs(sample[0]-original[original_index]) < accuracy
 	#test 2: Reading at fractional values, with this setup, is predictable.
-	halves= [0, 0.5, 1, 0.5]
-	fourths = [0, 0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25]
-	eighths = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 0.875, 0.75, 0.625, 0.5, 0.375, 0.25, 0.125]
-	correct = [halves, fourths, eighths]
 	for expected, denom  in zip(correct, (2,4,8)):
 		for i in xrange(len(expected)):
 			index = i/float(denom)
@@ -43,3 +52,16 @@ def test_table_get_sample():
 			sample = ffi.new("float[1]")
 			assert lav.Lav_tableGetSample(table, index, sample) == lav.Lav_ERROR_NONE
 			assert abs(sample[0]-expected[correct_index]) < accuracy
+
+def test_table_get_sample_range():
+	global halves, fourths, eighths, original, accuracy
+	table = make_table()
+	outarray = ffi.new("float[" + str(len(eighths)) + "]")
+	assert lav.Lav_tableComputeSampleRange(table, 0, 1, len(original), outarray) == lav.Lav_ERROR_NONE
+	for i in xrange(len(original)): assert abs(outarray[i]-original[i]) < accuracy
+	assert lav.Lav_tableComputeSampleRange(table, 0, 0.5, len(halves), outarray) == lav.Lav_ERROR_NONE
+	for i in xrange(len(halves)): assert abs(outarray[i]-halves[i]) < accuracy
+	assert lav.Lav_tableComputeSampleRange(table, 0, 0.25, len(fourths), outarray) == lav.Lav_ERROR_NONE
+	for i in xrange(len(fourths)): assert abs(outarray[i]-fourths[i]) < accuracy
+	assert lav.Lav_tableComputeSampleRange(table, 0, 1/8.0, len(eighths), outarray) == lav.Lav_ERROR_NONE
+	for i in xrange(len(eighths)): assert abs(outarray[i]-eighths[i]) < accuracy
