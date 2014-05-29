1cff0144334d43015e298069efc44686297344d0
Austin Hicks <camlorn38@gmail.com>
2014-05-29 15:05:47 -0400

    Program to time convolution
    This program estimates the maximum number of sources that can be run in realtime and
    in one thread by calculating
    5 seconds for one source through the HRTF.

--- src/utils/time_convolution.c
+++ src/utils/time_convolution.c
@@ -4,6 +4,50 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 
 /**Given a file, produces 5 seconds of audio output at 44100 HZ.  Rather than playing it, however, this program prints how long it took and the estimated number that can be run in one thread in realtime.*/
 #include <libaudioverse/private_all.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-void main() {
+#define seconds 5
+float storage[2*44100*seconds] = {0};
+
+void main(int argc, char** args) {
+	if(argc != 3) {
+		printf("Usage: %s <sound file> <hrtf file>", args[0]);
+		return;
+	}
+
+	LavGraph *graph;
+	LavNode* fileNode, *hrtfNode;
+	Lav_createGraph(SR, &graph);
+	LavError err = Lav_createFileNode(graph, args[1], &fileNode);
+	if(err != Lav_ERROR_NONE) {
+		printf("Error: %d", err);
+		return;
+	}
+
+	LavHrtfData *hrtf = NULL;
+	err = Lav_createHrtfData(args[2], &hrtf);
+	if(err != Lav_ERROR_NONE) {
+		printf("Error: %d", err);
+		return;
+	}
+	err = Lav_createHrtfNode(graph, hrtf, &hrtfNode);
+	if(err != Lav_ERROR_NONE) {
+		printf("Error: %d", err);
+		return;
+	}
+	Lav_setParent(hrtfNode, fileNode, 0, 0);
+	Lav_graphSetOutputNode(graph, hrtfNode);
+
+	printf("Convolving...\n");
+	clock_t start;
+	start = clock();
+	Lav_graphReadAllOutputs(graph, 44100*seconds, storage);
+	clock_t dur = clock()-start;
+	float secs = dur/(float)CLOCKS_PER_SEC;
+	printf("Done.\n");
+	printf("Convolution took %f seconds.\n", secs);
+	printf("Total number of sources possible in realtime: %f", (float)seconds/secs);
 }
