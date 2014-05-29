7b4f4c9253cd92a01f6d0b0ee300f1b368de76e1
Austin Hicks <camlorn38@gmail.com>
2014-05-29 14:34:28 -0400

    Setup for a program to estimate expense of convolution.

---
+++ src/utils/time_convolution.c
@@ -0,0 +1,9 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+
+/**Given a file, produces 5 seconds of audio output at 44100 HZ.  Rather than playing it, however, this program prints how long it took and the estimated number that can be run in one thread in realtime.*/
+#include <libaudioverse/private_all.h>
+
+void main() {
+}
