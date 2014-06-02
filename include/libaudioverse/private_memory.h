6fe520dbad925bacc3b61cc4c989ad83d219ef9e
Austin Hicks <camlorn38@gmail.com>
2014-06-02 14:14:13 -0400

    Declare types and functions for the memory manager

--- include/libaudioverse/private_memory.h
+++ include/libaudioverse/private_memory.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <uthash.h>
+#include "private_threads.h"
+
+typedef void (*LavFreeingFunction)(void*);
+
+struct LavAllocatedPointer {
+	void* id; //the pointer itself, named specially for uthash.
+	LavFreeingFunction free; //what to call with the pointer as argument to get rid of it and all memory associated with it.
+	UT_hash_handle hh; //magic internal field for uthash.
+};
+typedef struct LavAllocatedPointer LavAllocatedPointer;
+
+struct LavMemoryManager {
+	LavAllocatedPointer *managed_pointers;//through virtue of the magic of uthash, this is a hashtable.
+	void* lock; //opaque lock handle from threading module, prevents threads from clobbering the hashtable.
+};
+
+typedef struct LavMemoryManager LavMemoryManager;
+
+LavMemoryManager* createMemoryManager();
+//same as stdlib.
+void* managerAlloc(LavMemoryManager* manager, size_t size);
+void* managerCalloc(LavMemoryManager* manager, size_t elements, size_t size);
+void managerFree(LavMemoryManager* manager, void* ptr);
---
+++ src/libaudioverse/memory_manager.c
@@ -0,0 +1,10 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+#include <libaudioverse/private_all.h>
+#include <stdlib.h>
+#include <string.h>
+#include <utarray.h>
+#include <uthash.h>
+
+/**This file uses uthash to implement local and global memory contexts.*/
