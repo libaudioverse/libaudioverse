624b19ae5ea0b4d185b0d302c8c120d324d899d0
Austin Hicks <camlorn38@gmail.com>
2014-06-02 11:00:15 -0400

    Update tests for changes made in this branch

--- tests/test_node_integrity.py
+++ tests/test_node_integrity.py
@@ -6,7 +6,7 @@ def basic_node_checks(node):
 	assert node.properties is not ffi.NULL
 	assert node.graph is not ffi.NULL
 	for i in xrange(node.num_outputs):
-		assert node.outputs[i].samples is not ffi.NULL
+		assert node.outputs[i] is not ffi.NULL
 	for i in xrange(node.num_properties):
 		prop = node.properties[i]
 		assert prop.name is not ffi.NULL
@@ -25,7 +25,7 @@ def test_basic_node_integrity():
 	"""Initializes the most basic type of node, and sees if it looks valid."""
 	graph = ffi.new("LavGraph **")
 	node = ffi.new("LavNode **")
-	assert lav.Lav_ERROR_NONE == lav.Lav_createGraph(44100, graph)
+	assert lav.Lav_ERROR_NONE == lav.Lav_createGraph(44100, 128, graph)
 	assert lav.Lav_ERROR_NONE == lav.Lav_createNode(
 		5, #5 inputs.
 		3, #3 outputs.
@@ -37,6 +37,6 @@ def test_basic_node_integrity():
 def test_sine_node_integrity():
 	graph = ffi.new("LavGraph **")
 	node = ffi.new("LavNode **")
-	assert lav.Lav_ERROR_NONE == lav.Lav_createGraph(44100, graph)
+	assert lav.Lav_ERROR_NONE == lav.Lav_createGraph(44100, 128, graph)
 	assert lav.Lav_createSineNode(graph[0], node) == lav.Lav_ERROR_NONE
 	basic_node_checks(node[0])
