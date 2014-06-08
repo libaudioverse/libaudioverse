c347409374101059b1b7540308c137e1ac9063d3
Austin Hicks <camlorn38@gmail.com>
2014-06-08 19:03:03 -0400

    Tests run and pass on linux

--- make_cffi.py
+++ make_cffi.py
@@ -5,15 +5,19 @@ import sys
 input_file = 'include/libaudioverse/private_all.h'
 
 if sys.platform == 'win32':
-	command = 'cl /EP /DIS_TESTING ' + input_file
+	command = 'cl'
+	args = '/EP /DIS_TESTING ' + input_file
+	lib = 'libaudioverse.dll'
 else:
-	command  = 'cpp -D -D IS_TESTING ' + input_file
+	command  = 'cpp'
+	args = '-D IS_TESTING ' + input_file
+	lib = 'liblibaudioverse.so'
 
-src = subprocess.check_output(command)
+src = subprocess.check_output(command + ' ' + args, shell = True)
 src = src.replace("\r\n", "\n")
 src = src.replace("#pragma once", "")
 src = src.replace("__declspec(dllexport)", "")
 ffi = cffi.FFI()
 ffi.cdef(src)
 
-lav = ffi.dlopen("build/libaudioverse.dll")
+lav = ffi.dlopen("build/" + lib)
