73a2b3956383d5f4a3f058ec80e4a9aad9ca5a1b
Austin Hicks <camlorn38@gmail.com>
2014-06-10 00:09:36 -0400

    Time_convolution compiles again

--- src/utils/time_convolution.c
+++ src/utils/time_convolution.c
@@ -3,7 +3,7 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 
 /**Given a file, produces 5 seconds of audio output at 44100 HZ.  Rather than playing it, however, this program prints how long it took and the estimated number that can be run in one thread in realtime.*/
-#include <libaudioverse/private_all.h>
+#include <libaudioverse/libaudioverse.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,15 +19,15 @@ void main(int argc, char** args) {
 		return;
 	}
 
-	LavObject *graph;
+	LavDevice* device;
 	LavObject* fileNode, *hrtfNode;
 	LavError err = Lav_initializeLibrary();
 	if(err != Lav_ERROR_NONE) {
 		printf("Failed to initialize library. Error: %i", err);
 		return;
 	}
-	Lav_createGraph(SR, blocksize, &graph);
-	err = Lav_createFileNode(graph, args[1], &fileNode);
+	Lav_createReadDevice(blocksize, 2, 44100, &device);
+	err = Lav_createFileNode(device, args[1], &fileNode);
 	if(err != Lav_ERROR_NONE) {
 		printf("Error: %d", err);
 		return;
@@ -39,19 +39,19 @@ void main(int argc, char** args) {
 		printf("Error: %d", err);
 		return;
 	}
-	err = Lav_createHrtfNode(graph, hrtf, &hrtfNode);
+	err = Lav_createHrtfNode(device, hrtf, &hrtfNode);
 	if(err != Lav_ERROR_NONE) {
 		printf("Error: %d", err);
 		return;
 	}
 	Lav_setParent(hrtfNode, fileNode, 0, 0);
-	Lav_graphSetOutputNode(graph, hrtfNode);
+	Lav_deviceSetOutputObject(device, hrtfNode);
 
 	printf("Convolving...\n");
 	clock_t start;
 	start = clock();
 	for(unsigned int i = 0; i < 44100*seconds; i+= blocksize) {
-		Lav_graphGetBlock(graph, storage);
+		Lav_deviceGetBlock(device, storage);
 	}
 	clock_t dur = clock()-start;
 	float secs = dur/(float)CLOCKS_PER_SEC;
