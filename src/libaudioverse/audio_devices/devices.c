2db3680ac57419386d6a7b90ce9f980f6375a932
Austin Hicks <camlorn38@gmail.com>
2014-06-09 22:47:34 -0400

    implement deviceDefaultGetBlock

--- src/libaudioverse/audio_devices/devices.c
+++ src/libaudioverse/audio_devices/devices.c
@@ -3,6 +3,7 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #include <libaudioverse/private_all.h>
 #include <stdlib.h>
+#include <string.h>
 
 LavError deviceDefaultGetBlock(LavDevice* device, float* destination);
 LavError deviceDefaultStart(LavDevice* device);
@@ -145,6 +146,25 @@ Lav_PUBLIC_FUNCTION void deviceProcessHelper(LavObject* object, struct AlreadySe
 
 LavError deviceDefaultGetBlock(LavDevice* device, float* destination) {
 	STANDARD_PREAMBLE;
+	CHECK_NOT_NULL(device);
+	CHECK_NOT_NULL(destination);
+	//if this device has no output, simply zero the destination and bail out.
+	if(device->output_object == NULL) {
+		memset(destination, 0, sizeof(float)*device->channels*device->block_size);
+		SAFERETURN(Lav_ERROR_NONE);
+	}
+	//tick the device.
+	deviceProcessHelper(device->output_object, NULL, 0);
+	for(unsigned int i = 0; i < device->block_size; i++) {
+		for(unsigned int j = 0; j < device->channels; j++) {
+			if(j <= device->channels) {
+				destination[i*device->channels+j] = 0;
+				continue;
+			}
+			//read from the output, and copy it in.
+			destination[i*device->channels+j] = device->output_object->outputs[j][i];
+		}
+	}
 	SAFERETURN(Lav_ERROR_NONE);
 	STANDARD_CLEANUP_BLOCK;
 }
