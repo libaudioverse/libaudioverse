9121cbff1b541930f53a138a8d9c32baf60fc79a
Austin Hicks <camlorn38@gmail.com>
2014-06-11 20:25:51 -0400

    Minimal example of the 3d api

---
+++ src/examples/3d.c
@@ -0,0 +1,31 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+
+/**Demonstrates asynchronous audio output.*/
+#include <libaudioverse/private_all.h>
+#include <stdio.h>
+
+#define ERRCHECK(x) do {\
+if((x) != Lav_ERROR_NONE) {\
+	printf(#x " errored: %i", (x));\
+	return;\
+}\
+} while(0)\
+
+void main(int argc, char** args) {
+	if(argc != 2) {
+		printf("Usage:%s <souindfile> <hrtf>", args[0]);
+	}
+	char soundFile = args[1], hrtfFile = args[2];
+	LavDevice* device;
+	LavObject* node, *world, *source;
+	ERRCHECK(Lav_initializeLibrary());
+	ERRCHECK(Lav_createDefaultAudioOutputDevice(&device));
+	LavHrtfData* hrtf;
+	ERRCHECK(Lav_createHrtfData(hrtfFile, &hrtf));
+	ERRCHECK(Lav_createWorld(device, hrtf, &world));
+	ERRCHECK(Lav_createFileNode(device, soundFile, &node));
+	ERRCHECK(Lav_createMonoSource(node, world, &source));
+	sleepFor(2000);
+}
