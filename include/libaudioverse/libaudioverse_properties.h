8eb3b8999ed2071158d8532c6410eb73d2825743
Austin Hicks <camlorn38@gmail.com>
2014-06-12 20:27:30 -0400

    Removed the standard properties because they're not actually standard

--- include/libaudioverse/libaudioverse_properties.h
+++ include/libaudioverse/libaudioverse_properties.h
@@ -15,11 +15,6 @@ The standard, with the exception of the two standard properties, is:
 Lav_NODETYPE_PROPNAME
 */
 
-enum Lav_STDPROPERTIES {
-	Lav_STDPROPERTY_ADD = 0,
-	Lav_STDPROPERTY_MUL = 1,
-};
-
 enum Lav_SINE_PROPERTIES {
 	Lav_SINE_FREQUENCY = 0,
 };
