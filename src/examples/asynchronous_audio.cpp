329b5ed66d9c4b6105258a842ee5be33ae472670
Austin Hicks <camlorn38@gmail.com>
2014-06-19 15:21:02 -0400

    The asynchronous_audio example builds again

--- src/examples/asynchronous_audio.cpp
+++ src/examples/asynchronous_audio.cpp
@@ -3,7 +3,10 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 
 /**Demonstrates asynchronous audio output.*/
-#include <libaudioverse/private_all.h>
+#include <libaudioverse/libaudioverse.h>
+#include <libaudioverse/libaudioverse_properties.h>
+#include <thread>
+#include <chrono>
 #include <stdio.h>
 
 #define ERRCHECK(x) do {\
@@ -14,17 +17,16 @@ if((x) != Lav_ERROR_NONE) {\
 } while(0)\
 
 void main() {
-	
-	LavDevice* device;
+		LavDevice* device;
 	LavObject* node;
 	ERRCHECK(Lav_initializeLibrary());
 	ERRCHECK(Lav_createDefaultAudioOutputDevice(&device));
-	ERRCHECK(Lav_createSineNode(device, &node));
+	ERRCHECK(Lav_createSineObject(device, &node));
 	ERRCHECK(Lav_deviceSetOutputObject(device, node));
-	ERRCHECK(Lav_setFloatProperty(node, Lav_SINE_FREQUENCY, 0));
+	ERRCHECK(Lav_objectSetFloatProperty(node, Lav_SINE_FREQUENCY, 0));
 	for(unsigned int i = 0; i < 1000; i++) {
-		ERRCHECK(Lav_setFloatProperty(node, Lav_SINE_FREQUENCY, (float)i));
-		sleepFor(5);
+		ERRCHECK(Lav_objectSetFloatProperty(node, Lav_SINE_FREQUENCY, (float)i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-	sleepFor(2000);
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 }
