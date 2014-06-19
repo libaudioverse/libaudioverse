e16f7c074889290b0d1b7a424c834678b1999168
Austin Hicks <camlorn38@gmail.com>
2014-06-18 22:38:10 -0400

    Reimplemented Lav_createReadDevice

--- src/libaudioverse/audio_devices/device_negociation.cpp
+++ src/libaudioverse/audio_devices/device_negociation.cpp
@@ -8,7 +8,8 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 
 /**This file handles device negociation: figuring out which device is the best for the current platform.
 
-At the moment, this is always portaudio's default, and the setup is all done via the private API.*/
+At the moment, this is always portaudio's default, and the setup is all done via the private API.
+Read device creation is also here, because putting all the public device creation code in one place seemed like a good idea.*/
 
 Lav_PUBLIC_FUNCTION LavError Lav_createDefaultAudioOutputDevice(LavDevice** destination) {
 	//create a device if possible, giving it our desired settings, and then pass it to the portaudio thread builder, which redirects callbacks on the device as appropriate.
@@ -16,6 +17,13 @@ Lav_PUBLIC_FUNCTION LavError Lav_createDefaultAudioOutputDevice(LavDevice** dest
 	//we prefer 44100 sr, 2 channel, and block size of 1024 with mixahead 2.
 	//this gives a granularity of 23 MS between hearing property changes and about 69 MS between the app setting a property and the properety's new value being sent to the sound card.
 	retval = createPortaudioDevice(44100, 2, 1024, 2);
+	*destination = retval;
+	return Lav_ERROR_NONE;
+}
+
+Lav_PUBLIC_FUNCTION LavError Lav_createReadDevice(unsigned int sr, unsigned int channels, unsigned int blockSize, LavDevice** destination) {
+	LavDevice* retval = new LavDevice();
+	retval->init(sr, channels, blockSize, 0);
 	*destination = retval;
 	return Lav_ERROR_NONE;
 }
