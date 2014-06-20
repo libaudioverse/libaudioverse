51530d7f84b1570323422ccaf0b11ad8c8503992
Austin Hicks <camlorn38@gmail.com>
2014-06-20 17:11:29 -0400

    Interface for class that knows how to read files

---
+++ include/libaudioverse/private_file.hpp
@@ -0,0 +1,24 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+#pragma once
+#include <sndfile.h>
+
+/**A completely stand-alone wrapper around Libsndfile for file reading: implements both a  streaming and non-streaming interface.
+
+This can theoreticaly handle any type of file, including codec decoding, but uses Libsndfile for the moment.*/
+class LavFileReader {
+	public:
+	LavFileReader() = default;
+	LavError open();
+	LavError close();
+	float getSampleRate();
+	unsigned int getChannels();
+	unsigned int getFrames();
+	unsigned int getLengthInSamples();
+	unsigned int readAll(float* buffer);
+	unsigned int read(unsigned int frames, float* buffer);
+	protected:
+	SNDFILE* handle;
+	SF_INFO info;
+};
