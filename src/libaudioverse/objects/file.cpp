ecf3c34a703a4d995bd0f016f4cae6ae59e8cb2c
Austin Hicks <camlorn38@gmail.com>
2014-06-20 19:24:30 -0400

    Implemented file::process. Does not yet provide seeking/pitch bend again as of yet.

--- src/libaudioverse/objects/file.cpp
+++ src/libaudioverse/objects/file.cpp
@@ -9,9 +9,11 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <libaudioverse/libaudioverse.h>
 #include <libaudioverse/libaudioverse_properties.h>
 #include <libaudioverse/private_macros.hpp>
+#include <libaudioverse/private_dspmath.hpp>
 
 class LavFileObject: public LavObject {
 	virtual void init(LavDevice* device, const char* path);
+	virtual void process();
 	protected:
 	LavFileReader file;
 	float* buffer = nullptr;
@@ -27,4 +29,26 @@ void LavFileObject::init(LavDevice* device, const char* path) {
 	buffer = new float[file.getSampleCount()];
 	file.readAll(buffer);
 	delta = file.getSr()/device->getSr();
+}
+
+void LavFileObject::process() {
+	for(unsigned int i = 0; i < device->getBlockSize(); i++) {
+		if(offset >= file.getFrameCount()) {
+			for(unsigned int j = 0; j < num_outputs; j++) {
+				outputs[j][i] = 0.0f;
+			}
+		}
+		unsigned int samp1 = (unsigned int)offset;
+		unsigned int samp2 = (unsigned int)offset+1;
+		float weight1 = 1-offset;
+		float weight2 = offset;
+		for(unsigned int j = 0; j < num_outputs; j++) {
+			unsigned int ind1 = samp1*num_outputs+j;
+			unsigned int ind2 = samp2*num_outputs+j;
+			outputs[j][i] = weight1*buffer[ind1]+weight2*buffer[ind2];
+		}		
+	offset += delta;
+	position += (unsigned int)offset;
+	offset = ringmodf(offset, 1.0f);
+	}
 }
