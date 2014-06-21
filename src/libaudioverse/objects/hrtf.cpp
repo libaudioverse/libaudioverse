3224b00bfb190eb050ec4b42a75417237b2ec349
Austin Hicks <camlorn38@gmail.com>
2014-06-20 23:09:16 -0400

    Changing elevation and azimuth now marks hrtf node as needing to perform a recompute of the hrir.

--- src/libaudioverse/objects/hrtf.cpp
+++ src/libaudioverse/objects/hrtf.cpp
@@ -11,6 +11,7 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <libaudioverse/private_macros.hpp>
 #include <libaudioverse/private_hrtf.hpp>
 #include <limits>
+#include <functional>
 
 class LavHrtfObject: public LavObject {
 	public:
@@ -19,6 +20,7 @@ class LavHrtfObject: public LavObject {
 	private:
 	float *history, *left_response, *right_response;
 	LavHrtfData* hrtf;
+	bool needs_hrtf_recompute;
 };
 
 void LavHrtfObject::init(LavDevice* device, LavHrtfData* hrtf) {
@@ -30,4 +32,7 @@ void LavHrtfObject::init(LavDevice* device, LavHrtfData* hrtf) {
 	hrtf->computeCoefficientsStereo(0.0f, 0.0f, left_response, right_response);
 	properties[Lav_HRTF_AZIMUTH] = createFloatProperty("azimuth", 0.0f, -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
 	properties[Lav_HRTF_ELEVATION] = createFloatProperty("elevation", 0.0f, -90.0f, 90.0f);
+	auto markRecompute = [this](){needs_hrtf_recompute = true;};
+	properties[Lav_HRTF_AZIMUTH]->setPostChangedCallback(markRecompute);
+	properties[Lav_HRTF_ELEVATION]->setPostChangedCallback(markRecompute);
 }
