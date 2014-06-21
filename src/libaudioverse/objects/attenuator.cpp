e901ddcfb474cf3425613f35a404d38469df4ce3
Austin Hicks <camlorn38@gmail.com>
2014-06-20 21:14:04 -0400

    Reimplemented attenuator node as a class

--- src/libaudioverse/objects/attenuator.cpp
+++ src/libaudioverse/objects/attenuator.cpp
@@ -1,40 +1,36 @@
 /**Copyright (C) Austin Hicks, 2014
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+#include <libaudioverse/libaudioverse.h>
+#include <libaudioverse/libaudioverse_properties.h>
+#include <libaudioverse/private_devices.hpp>
+#include <libaudioverse/private_objects.hpp>
+#include <libaudioverse/private_properties.hpp>
+#include <libaudioverse/private_macros.hpp>
+#include <limits>
 
-#include <math.h>
-#include <stdlib.h>
-#include <libaudioverse/private_all.hpp>
+class LavAttenuatorObject: public LavObject {
+	public:
+	virtual void init(LavDevice* device, unsigned int numChannels);
+	virtual void process();
+};
 
-LavError attenuatorProcessor(LavObject *node);
-
-Lav_PUBLIC_FUNCTION LavError Lav_createAttenuatorNode(LavDevice* device, unsigned int numInputs, LavObject **destination) {
-	STANDARD_PREAMBLE;
-	LavError err = Lav_ERROR_NONE;
-	CHECK_NOT_NULL(destination);
-	CHECK_NOT_NULL(device);
-	ERROR_IF_TRUE(numInputs <= 0, Lav_ERROR_RANGE);
-	LOCK(device->mutex);
-	LavNode *retval = NULL;
-	err = Lav_createNode(numInputs, numInputs, 
-sizeof(attenuatorPropertyTable)/sizeof(attenuatorPropertyTable[0]), attenuatorPropertyTable,
-Lav_NODETYPE_ATTENUATOR, device, (LavObject**)&retval);
-	if(err != Lav_ERROR_NONE) SAFERETURN(err);
-
-	((LavObject*)retval)->process = attenuatorProcessor;
-	*destination = (LavObject*)retval;
-	SAFERETURN(Lav_ERROR_NONE);
-	STANDARD_CLEANUP_BLOCK;
+void LavAttenuatorObject::init(LavDevice* device, unsigned int numChannels) {
+	LavObject::init(device, numChannels, numChannels);
+	properties[Lav_ATTENUATOR_MULTIPLIER] = createFloatProperty("multiplier", 1.0f, -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
 }
 
-LavError attenuatorProcessor(LavObject *obj) {
-	const LavNode* node = (LavNode*)obj;
-	float mul = 0;
-	Lav_getFloatProperty((LavObject*)node, Lav_ATTENUATOR_MULTIPLIER, &mul);
-	for(unsigned int i = 0; i < obj->device->block_size; i++) {
-		for(unsigned int o = 0; o < obj->num_outputs; o++) {
-			obj->outputs[o][i] = obj->inputs[o][i]*mul;
+LavObject* createAttenuatorObject(LavDevice* device, unsigned int numChannels) {
+	auto retval = new LavAttenuatorObject();
+	retval->init(device, numChannels);
+	return retval;
+}
+
+void LavAttenuatorObject::process() {
+	const float mul = properties[Lav_ATTENUATOR_MULTIPLIER]->getFloatValue();
+	for(unsigned int i = 0; i < device->getBlockSize(); i++) {
+		for(unsigned int j = 0; j < num_outputs; j++) {
+			outputs[j][i] = inputs[j][i]*mul;
 		}
 	}
-	return Lav_ERROR_NONE;
 }
