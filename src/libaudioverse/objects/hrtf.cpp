9d38f78dd95e624d0c0bbffa3c0a2da4049c13fa
Austin Hicks <camlorn38@gmail.com>
2014-06-20 23:32:52 -0400

    Private api for hrtf objects

--- src/libaudioverse/objects/hrtf.cpp
+++ src/libaudioverse/objects/hrtf.cpp
@@ -1,8 +1,6 @@
 /**Copyright (C) Austin Hicks, 2014
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
-#include <stdlib.h>
-#include <string.h>
 #include <libaudioverse/libaudioverse.h>
 #include <libaudioverse/libaudioverse_properties.h>
 #include <libaudioverse/private_devices.hpp>
@@ -12,14 +10,15 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <libaudioverse/private_hrtf.hpp>
 #include <limits>
 #include <functional>
+#include <algorithm>
 
 class LavHrtfObject: public LavObject {
 	public:
 	virtual void init(LavDevice* device, LavHrtfData* hrtf);
 	virtual void process();
 	private:
-	float *history, *left_response, *right_response;
-	LavHrtfData* hrtf;
+	float *history = nullptr, *left_response = nullptr, *right_response = nullptr;
+	LavHrtfData* hrtf = nullptr;
 	bool needs_hrtf_recompute;
 };
 
@@ -35,4 +34,27 @@ void LavHrtfObject::init(LavDevice* device, LavHrtfData* hrtf) {
 	auto markRecompute = [this](){needs_hrtf_recompute = true;};
 	properties[Lav_HRTF_AZIMUTH]->setPostChangedCallback(markRecompute);
 	properties[Lav_HRTF_ELEVATION]->setPostChangedCallback(markRecompute);
+}
+
+void LavHrtfObject::process() {
+	//calculating the hrir is expensive, do it only if needed.
+	if(needs_hrtf_recompute) {
+		const float elev = properties[Lav_HRTF_ELEVATION]->getFloatValue();
+		const float az = properties[Lav_HRTF_AZIMUTH]->getFloatValue();
+		hrtf->computeCoefficientsStereo(elev, az, left_response, right_response);
+		needs_hrtf_recompute = false;
+	}
+	float *start = history+hrtf->getLength(), *end = history+hrtf->getLength()+device->getBlockSize();
+	//roll back the history...
+	std::copy(end-hrtf->getLength(), end, history);
+	//stick our input on the end...
+	std::copy(inputs[0], inputs[0]+device->getBlockSize(), start);
+	//finally, do the usual convolution loop.
+	for(unsigned int i = 0; i < device->getBlockSize(); i++, start++) {
+		float outLeft = 0, outRight = 0;
+		for(unsigned int j = 00; j < hrtf->getLength(); j++) {
+			outLeft = left_response[j]* *(start-j);
+			outRight = right_response[j]* *(start-j);
+		}
+	}
 }
