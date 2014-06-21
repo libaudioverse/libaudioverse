65a0237938fb23fecdc0f4342ab48cac7c8edc8d
Austin Hicks <camlorn38@gmail.com>
2014-06-21 13:52:11 -0400

    Defined LavErrorException, an exception for the public api code paths to make errors bubble up appropriately to the user

--- include/libaudioverse/private_errors.hpp
+++ include/libaudioverse/private_errors.hpp
@@ -2,8 +2,11 @@
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #pragma once
-#include <mutex>
-#include "private_errors.hpp" //needed by the standard catchblock macro, below.
+#include "libaudioverse.h"
 
-//use __LINE__ for a quick unique variable.
-#define LOCK(object) std::lock_guard<decltype((object))> lock_##__LINE__((object))
+/*This class serves one purpose: preventing us from catching and throwing integers.*/
+class LavErrorException {
+	public:
+	LavErrorException(LavError e): err(e) {}
+	LavError err;
+};
