a2eac2bef6fdec6d5950256f13ad5e6bd341d65d
Austin Hicks <camlorn38@gmail.com>
2014-06-23 22:57:29 -0400

    The 3d example builds again

--- src/examples/3d.cpp
+++ src/examples/3d.cpp
@@ -2,9 +2,13 @@
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 
-/**Demonstrates asynchronous audio output.*/
-#include <libaudioverse/private_all.h>
+/**Demonstrates 3d api.*/
+#include <libaudioverse/libaudioverse.h>
+#include <libaudioverse/libaudioverse_properties.h>
+#include <libaudioverse/libaudioverse3d.h>
 #include <stdio.h>
+#include <thread>
+#include <chrono>
 
 #define ERRCHECK(x) do {\
 if((x) != Lav_ERROR_NONE) {\
@@ -15,13 +19,14 @@ if((x) != Lav_ERROR_NONE) {\
 
 /**Explanation: distance models are 0, 1, and 2.  This function uses that to give us the next one, given the current.  Not great practice.  For testing only.*/
 int calcNextDistanceModel(int model) {
-	model = (model+1)%3;
+	return Lav_DISTANCE_MODEL_LINEAR; //temporary: all others are disabled atm.
+/*	model = (model+1)%3;
 	switch(model) {
 		case Lav_DISTANCE_MODEL_LINEAR: printf("Using Linear distance model.\n"); break;
 		case Lav_DISTANCE_MODEL_EXPONENTIAL: printf("Using exponential distance model.\n"); break;
 		case Lav_DISTANCE_MODEL_INVERSE_SQUARE: printf("Using inverse square distance model.\n"); break;
 	}
-	return model;
+	return model;*/
 }
 
 void main(int argc, char** args) {
@@ -37,8 +42,8 @@ void main(int argc, char** args) {
 	LavHrtfData* hrtf;
 	ERRCHECK(Lav_createHrtfData(hrtfFile, &hrtf));
 	ERRCHECK(Lav_createWorld(device, hrtf, &world));
-	ERRCHECK(Lav_createFileNode(device, soundFile, &node));
-	ERRCHECK(Lav_createMonoSource(node, world, &source));
+	ERRCHECK(Lav_createFileObject(device, soundFile, &node));
+	ERRCHECK(Lav_createSource(device, world, node, &source));
 	const int resolution = 1000, length = 3000; //length in ms.
 	const float width = 30.0;
 	//do a square over and over.
@@ -46,23 +51,22 @@ void main(int argc, char** args) {
 	printf("Using linear distance model.\n");
 	while(1) {
 		for(int i = 0; i < resolution; i++) {
-			Lav_setFloat3Property(source, Lav_SOURCE_POSITION, width*(i/(float)resolution)-width/2, 0, -width/2);
-			sleepFor(length/resolution);
+			Lav_objectSetFloat3Property(source, Lav_3D_POSITION, width*(i/(float)resolution)-width/2, 0, -width/2);
+			std::this_thread::sleep_for(std::chrono::milliseconds(length/resolution));
 		}
 		for(int i = 0; i < resolution; i++) {
-			Lav_setFloat3Property(source, Lav_SOURCE_POSITION, width/2, 0, (width*((float)i/resolution)-width/2) );
-			sleepFor(length/resolution);
+			Lav_objectSetFloat3Property(source, Lav_3D_POSITION, width/2, 0, (width*((float)i/resolution)-width/2) );
+			std::this_thread::sleep_for(std::chrono::milliseconds(length/resolution));
 		}
 		for(int i = 0; i < resolution; i++) {
-			Lav_setFloat3Property(source, Lav_SOURCE_POSITION, -(width*(i/(float)resolution)-width/2), 0, width/2);
-			sleepFor(length/resolution);
+			Lav_objectSetFloat3Property(source, Lav_3D_POSITION, -(width*(i/(float)resolution)-width/2), 0, width/2);
+			std::this_thread::sleep_for(std::chrono::milliseconds(length/resolution));
 		}
 		for(int i = 0; i < resolution; i++) {
-			Lav_setFloat3Property(source, Lav_SOURCE_POSITION, -width/2, 0, -(width*((float)i/resolution)-width/2));
-			sleepFor(length/resolution);
+			Lav_objectSetFloat3Property(source, Lav_3D_POSITION, -width/2, 0, -(width*((float)i/resolution)-width/2));
+			std::this_thread::sleep_for(std::chrono::milliseconds(length/resolution));
 		}
 		model = calcNextDistanceModel(model);
-		Lav_setIntProperty(source, Lav_SOURCE_DISTANCE_MODEL, model);
+		Lav_objectSetIntProperty(source, Lav_SOURCE_DISTANCE_MODEL, model);
 	}
-	sleepFor(2000);
 }
