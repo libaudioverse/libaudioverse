33a714b2e5912bd921bc70f1bbd64529d384a38c
Austin Hicks <camlorn38@gmail.com>
2014-06-24 21:05:49 -0400

    Removed the tests.
    Now that we're c++, this doesn't actually work, or even particularly help things.

--- tests/conf.py
+++
@@ -1,3 +0,0 @@
-#various configuration variables that end up appearing all over the place.  Mainly accuracies.
-#this is the lowest allowed difference between two floating point numbers before they are considered unequal.
-accuracy = 1e-3
