97fed5fb1e03516a7f99d39c1002ca7684cb5a2a
Austin Hicks <camlorn38@gmail.com>
2014-06-28 12:13:44 -0400

    Logic for making friendly exception names

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -20,6 +20,16 @@ def make_python(info):
 		friendly_name =  re.sub('[A-Z]', lambda x: '_' + x.group(0).lower(), without_lav)
 		friendly_functions[name] = friendly_name
 	context['friendly_functions'] = friendly_functions
+	#similar logic for error constants, only this time we're converting them to python class names.
+	friendly_constants = OrderedDict()
+	for i in [j for j in info['constants'].iterkeys() if j.startswith('Lav_ERROR_')]:
+		if i == 'Lav_ERROR_NONE':
+			continue #this doesn't map to an exception.
+		friendly_name = i.lower()
+		friendly_name = re.sub('_([a-z])', lambda x: x.group(1).upper(), friendly_name)
+		friendly_name = friendly_name[len('lavError'):] #strip leading lavError, left over from the above conversion.
+		friendly_name += 'Error' #tag the end of it for the class names.
+		print friendly_name
 	env = jinja2.Environment(loader = jinja2.PackageLoader(__package__, ""), undefined = jinja2.StrictUndefined)
 	return {
 		'_lav.py' : env.get_template('_lav.py.t').render(context),
