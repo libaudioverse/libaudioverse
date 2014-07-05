d7459ded6e19afcd0db1985584355e3e37c93dc3
Austin Hicks <camlorn38@gmail.com>
2014-07-05 15:47:02 -0400

    Update device_negociation

--- src/libaudioverse/audio_devices/device_negociation.cpp
+++ src/libaudioverse/audio_devices/device_negociation.cpp
@@ -3,6 +3,8 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #include <libaudioverse/libaudioverse.h>
 #include <libaudioverse/private_devices.hpp>
+#include <libaudioverse/private_memory.hpp>
+#include <libaudioverse/private_macros.hpp>
 #include <stdlib.h>
 #include <string.h>
 
@@ -16,13 +18,14 @@ Lav_PUBLIC_FUNCTION LavError Lav_createDefaultAudioOutputDevice(LavDevice** dest
 	LavDevice* retval;
 	//we prefer 44100 sr, 2 channel, and block size of 1024 with mixahead 2.
 	//this gives a granularity of 23 MS between hearing property changes and about 69 MS between the app setting a property and the properety's new value being sent to the sound card.
-	retval = createPortaudioDevice(44100, 2, 1024, 2);
+	retval = outgoingPointer<LavDevice>(createPortaudioDevice(44100, 2, 1024, 2));
 	*destination = retval;
 	return Lav_ERROR_NONE;
 }
 
 Lav_PUBLIC_FUNCTION LavError Lav_createReadDevice(unsigned int sr, unsigned int channels, unsigned int blockSize, LavDevice** destination) {
-	LavDevice* retval = new LavDevice(sr, channels, blockSize, 0);
-	*destination = retval;
-	return Lav_ERROR_NONE;
+	PUB_BEGIN
+	auto shared = std::make_shared<LavDevice>(sr, channels, blockSize, 0);
+	*destination = outgoingPointer(shared);
+	PUB_END
 }
