99da3024ede610e55a4ff1d007fcca58fe108830
Austin Hicks <camlorn38@gmail.com>
2014-07-08 17:47:06 -0400

    Fixed the delay line to not crash, but it doesn't actually seem to be delaying for some reason

--- src/libaudioverse/objects/delay.cpp
+++ src/libaudioverse/objects/delay.cpp
@@ -8,6 +8,7 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <libaudioverse/private_properties.hpp>
 #include <libaudioverse/private_macros.hpp>
 #include <libaudioverse/private_memory.hpp>
+#include <libaudioverse/private_dspmath.hpp>
 #include <limits>
 #include <memory>
 
@@ -19,10 +20,12 @@ class LavDelay: public LavObject {
 	void maxDelayChanged();
 	protected:
 	std::vector<float*> delay_lines;
-	unsigned int delay_line_length;
+	int delay_line_length = 0;
+	int write_pos = 0, line_count = 0;
 };
 
 LavDelay::LavDelay(std::shared_ptr<LavDevice> device, unsigned int lines): LavObject(Lav_OBJTYPE_DELAY, device, lines, lines) {
+	line_count = lines;
 	if(lines == 0) throw LavErrorException(Lav_ERROR_RANGE);
 	getProperty(Lav_DELAY_DELAY_MAX).setPostChangedCallback([this] () {maxDelayChanged();});
 	maxDelayChanged(); //delegate the allocation.
@@ -41,7 +44,6 @@ LavDelay::~LavDelay() {
 }
 
 void LavDelay::maxDelayChanged() {
-	unsigned int lineCount = delay_lines.size();
 	for(auto i: delay_lines) {
 		delete[] i;
 	}
@@ -56,27 +58,31 @@ void LavDelay::maxDelayChanged() {
 	unsigned int delayLineLength = (unsigned int)(maxDelay*device->getSr());
 	delay_line_length = delayLineLength;
 	//reallocate the lines.
-	for(unsigned int i = 0; i < lineCount; i++) {
-		float* tmp = new float[delay_line_length];
+	for(int i = 0; i < line_count; i++) {
+		float* tmp = new float[delay_line_length]();
 		delay_lines.push_back(tmp);
 	}
 }
 
 void LavDelay::process() {
 	float delay = getProperty(Lav_DELAY_DELAY).getFloatValue();
-	unsigned int sampleTarget = (unsigned int)floorf(delay);
+	int readOffset = (unsigned int)floorf(delay);
 	float offset = delay-floorf(delay);
-	unsigned int samp1 = sampleTarget;
-	unsigned int samp2 = samp1+1;
-	//we can't wrap past the end; if the delay is past the last sample, we need to just cut it off.
-	//this can't happen often, but floating point is a nightmare and this is one of those cases.
-		if(samp2 > delay_line_length) samp2 = samp1;
-	//the weights.
-	float weight1 = 1-offset, weight2 = offset;
-	for(unsigned output = 0; output < num_outputs; output++) {
-		for(unsigned int i = 0; i < block_size; i++) {
+	//this prevents us from actually trying to read the future.
+	if(readOffset >= delay_line_length) {
+		readOffset = delay_line_length-1;
+	}
+	for(unsigned int i = 0; i < block_size; i++) {
+		int samp1 = -readOffset, samp2 = -(readOffset+1);
+		samp1 = ringmodi(samp1+write_pos, delay_line_length);
+		samp2 = ringmodi(samp2+write_pos, delay_line_length);
+		float weight1 = 1.0f-offset;
+		float weight2 = offset;
+		for(unsigned int output = 0; output < num_outputs; output++) {
+			delay_lines[output][write_pos] = inputs[output][i];
 			outputs[output][i] = weight1*delay_lines[output][samp1]+weight2*delay_lines[output][samp2];
 		}
+		write_pos = ringmodi(write_pos+1, delay_line_length);
 	}
 }
 
