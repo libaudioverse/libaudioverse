3e01eaf73d62139f8c97fc98dceef6a8a59efa26
Austin Hicks <camlorn38@gmail.com>
2014-07-08 17:12:03 -0400

    Built but did not expose a delay line

--- src/libaudioverse/objects/delay.cpp
+++ src/libaudioverse/objects/delay.cpp
@@ -11,3 +11,64 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <limits>
 #include <memory>
 
+class LavDelay: public LavObject {
+	LavDelay(std::shared_ptr<LavDevice> device, unsigned int lines);
+	~LavDelay();
+	void process();
+	void maxDelayChanged();
+	protected:
+	std::vector<float*> delay_lines;
+	unsigned int delay_line_length;
+};
+
+LavDelay::LavDelay(std::shared_ptr<LavDevice> device, unsigned int lines): LavObject(Lav_OBJTYPE_DELAY, device, lines, lines) {
+	if(lines == 0) throw LavErrorException(Lav_ERROR_RANGE);
+	getProperty(Lav_DELAY_DELAY_MAX).setPostChangedCallback([this] () {maxDelayChanged();});
+	maxDelayChanged(); //delegate the allocation.
+}
+
+LavDelay::~LavDelay() {
+	for(auto i: delay_lines) {
+		delete[] i;
+	}
+}
+
+void LavDelay::maxDelayChanged() {
+	unsigned int lineCount = delay_lines.size();
+	for(auto i: delay_lines) {
+		delete[] i;
+	}
+	delay_lines.clear();
+	float delay = getProperty(Lav_DELAY_DELAY).getFloatValue();
+	float maxDelay = getProperty(Lav_DELAY_DELAY_MAX).getFloatValue();
+	if(maxDelay < delay) {
+		delay = maxDelay;
+		getProperty(Lav_DELAY_DELAY).setFloatValue(delay);
+	}
+	getProperty(Lav_DELAY_DELAY).setFloatRange(0.0f, maxDelay);
+	unsigned int delayLineLength = (unsigned int)(maxDelay*device->getSr());
+	delay_line_length = delayLineLength;
+	//reallocate the lines.
+	for(unsigned int i = 0; i < lineCount; i++) {
+		float* tmp = new float[delay_line_length];
+		delay_lines.push_back(tmp);
+	}
+}
+
+void LavDelay::process() {
+	float delay = getProperty(Lav_DELAY_DELAY).getFloatValue();
+	unsigned int sampleTarget = (unsigned int)floorf(delay);
+	float offset = delay-floorf(delay);
+	unsigned int samp1 = sampleTarget;
+	unsigned int samp2 = samp1+1;
+	//we can't wrap past the end; if the delay is past the last sample, we need to just cut it off.
+	//this can't happen often, but floating point is a nightmare and this is one of those cases.
+		if(samp2 > delay_line_length) samp2 = samp1;
+	//the weights.
+	float weight1 = 1-offset, weight2 = offset;
+	for(unsigned output = 0; output < num_outputs; output++) {
+		for(unsigned int i = 0; i < block_size; i++) {
+			outputs[output][i] = weight1*delay_lines[output][samp1]+weight2*delay_lines[output][samp2];
+		}
+	}
+}
