0b32c59f83543f6ba0b0adf37d1ff6695ff5259d
Austin Hicks <camlorn38@gmail.com>
2014-07-14 16:12:27 -0400

    Filter to convert an identifier to camelcase

--- bindings/transformers.py
+++ bindings/transformers.py
@@ -1,6 +1,7 @@
 """Implements standard transformers: Lav_ remover, camelcase-to-underscore and back, etc.
 
 These are then made available to templates."""
+import re
 
 def without_lav(s):
 	"""Remove the lav_ prefix from a Libaudioverse identifier."""
@@ -8,6 +9,14 @@ def without_lav(s):
 		return s[4:]
 	else:
 		return s
+
+def camelize(s, capitalize_first = False):
+	"""Convert an identifer of the form ab_cd_ef_gh to abCdEfGh.  If capitalize_first is True, convert to AbCdEfGh."""
+	what = s.lower()
+	what = re.sub('_([a-z])', lambda x: x.group(1).upper(), what)
+	if capitalize_first:
+		what = what[0].upper() + what[1:]
+	return what
 
 def get_jinja2_filters():
 	return {'without_lav': without_lav}
