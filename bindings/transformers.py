545fed87c6fc3792b85e3bc26966dc683b7dace8
Austin Hicks <camlorn38@gmail.com>
2014-07-14 16:05:46 -0400

    Filter to remove lav_ from identifiers

--- bindings/transformers.py
+++ bindings/transformers.py
@@ -2,5 +2,12 @@
 
 These are then made available to templates."""
 
+def without_lav(s):
+	"""Remove the lav_ prefix from a Libaudioverse identifier."""
+	if s.startswith('Lav_'):
+		return s[4:]
+	else:
+		return s
+
 def get_jinja2_filters():
-	return dict()
+	return {'without_lav': without_lav}
