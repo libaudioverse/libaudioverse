0ed2ff5bacd0bd5bfd4ec450d9684550bd78f883
Austin Hicks <camlorn38@gmail.com>
2014-07-19 17:09:02 -0400

    Added back in enable_shared_from this: callbacks must be able to extend the lifetimes of objects slightly

--- include/libaudioverse/private_objects.hpp
+++ include/libaudioverse/private_objects.hpp
@@ -18,7 +18,7 @@ class LavInputDescriptor {
 };
 
 /**Things all Libaudioverse objects have.*/
-class LavObject {
+class LavObject: public std::enable_shared_from_this<LavObject> { //enable_shared_from_this is for callback infrastructure.
 	public:
 	LavObject(int type, std::shared_ptr<LavDevice> device, unsigned int numInputs, unsigned int numOutputs);
 	virtual ~LavObject();
