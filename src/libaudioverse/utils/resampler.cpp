cae8c803aa56cb7ecddd530f99e2a95257fe865b
Austin Hicks <camlorn38@gmail.com>
2014-07-25 17:12:28 -0400

    Gut the resampler. It needs a better interface

--- src/libaudioverse/utils/resampler.cpp
+++ src/libaudioverse/utils/resampler.cpp
@@ -11,35 +11,4 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <string.h>
 #include <stdio.h>
 #include <libaudioverse/private_dspmath.hpp>
-LavResampler::LavResampler(int sourceSr, int targetSr, std::function<std::tuple<int, float*>(void)> getFromCallback): source_sr(sourceSr), target_sr(targetSr), get_from_callback(getFromCallback) {
-	delta = (float)sourceSr/(float)targetSr;
-}
-float LavResampler::getNext() {
-	if(current_pos >= std::get<0>(current_pair)-1) {
-		last_sample = std::get<1>(current_pair)[std::get<0>(current_pair)];
-		current_pair = get_from_callback();
-		current_pos = 0;
-	}
-	float w1 = 1-current_offset;
-	float w2 = current_offset;
-	float s1, s2;
-	if(current_pos == 0) {
-		s1 = last_sample;
-		s2 = std::get<1>(current_pair)[0];
-	}
-	else {
-		s1 = std::get<1>(current_pair)[current_pos];
-		s2 = std::get<1>(current_pair)[current_pos+1];
-	}
-	//update current_pos and current_offset.
-	current_offset += delta;
-	current_pos += (int)floorf(current_offset);
-	current_offset = current_offset-floorf(current_offset);
-	return w1*s1+w2*s2;
-}
 
-void LavResampler::read(int count, float* output) {
-	for(int i = 0; i < count; i++) {
-		output[i] = getNext();
-	}
-}
