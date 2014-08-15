01a612416742ea72852a9372317180abfa39c510
Austin Hicks <camlorn38@gmail.com>
2014-08-15 12:59:52 -0400

    Add an as-of-yet nonfunctional example of 3d simulation to the Python bindings

---
+++ bindings/python/examples/sim3d.py
@@ -0,0 +1,23 @@
+#demonstrates how to use the 3d simulation.
+import libaudioverse
+
+dev = libaudioverse.Device(physical_output_index = -1)
+world = libaudioverse.WorldObject(dev, "mit.hrtf")
+source = libaudioverse.SourceObject(dev, world)
+print """Enter a path to a sound file.
+For best results, this should be mono.  If not, only the first (usually left) channel will be used."""
+filepath = raw_input()
+f = libaudioverse.FileObject(filepath)
+source.inputs[0] = f, 0
+dev.output_object = world
+print """Enter python expressions that evaluate to 3-tuples (x, y, z).
+Positive x is to your right, positive y is above you, and positive z is behind you.
+Enter quit to quit."""
+while True:
+	command = raw_input()
+	if command == 'quit':
+		break
+	vect = eval(command)
+	if len(vect) != 3:
+		print "Must evaluate to a 3-tuple.  Try again"
+	source.position = vect
