fd72553b7ec14b4d24f46034f493f8266b0402a3
Austin Hicks <camlorn38@gmail.com>
2014-08-14 20:29:47 -0400

    Refactor bindings geerator and add support for extra directories

--- bindings/make_bindings.py
+++ bindings/make_bindings.py
@@ -8,25 +8,31 @@ generators = {
 'python' : make_python,
 }
 
-def write_files(files, binding_dir):
-	if os.path.exists(binding_dir):
-		shutil.rmtree(binding_dir)
+def write_files(files, source_dir, dest_dir):
+	special_keys = ['dll_location', 'additional_dirs']
+	if os.path.exists(dest_dir):
+		shutil.rmtree(dest_dir)
 	for name, contents in files.iteritems():
-		if name == 'dll_location': #the special key that points at where we want the dll to go.
-			path = os.path.join(binding_dir, contents)
-			dir = os.path.split(path)[0]
-			if not os.path.exists(dir):
-				os.makedirs(dir)
-			shutil.copy(os.path.join(get_info.root_directory, 'build', 'libaudioverse.dll'), path)
+		if name in special_keys:
 			continue
-		fullpath = os.path.join(binding_dir, name)
+		fullpath = os.path.join(dest_dir, name)
 		dir = os.path.split(fullpath)[0]
 		if not os.path.exists(dir):
 			os.makedirs(dir)
 		with file(fullpath, 'w') as f:
 			f.write(contents)
+	#handle the dll's copying.
+	dll_target = files.get('dll_location', 'libaudioverse.dll')
+	path = os.path.join(dest_dir, dll_target)
+	dir = os.path.split(path)[0]
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+	shutil.copy(os.path.join(get_info.root_directory, 'build', 'libaudioverse.dll'), path)
+	#copy additional directories
+	for i in files.get('additional_directories', []):
+		shutil.copytree(os.path.join(source_dir, i), os.path.join(dest_dir, i))
 
 def make_bindings():
 	for name, func in generators.iteritems():
 		files = func(get_info.all_info)
-		write_files(files, os.path.join(get_info.root_directory, 'build', 'bindings', name))
+		write_files(files, os.path.join(get_info.root_directory, 'bindings', name), os.path.join(get_info.root_directory, 'build', 'bindings', name))
