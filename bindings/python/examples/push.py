fe772734aa7526cebff24041cc38c7a87797f3fd
Austin Hicks <camlorn38@gmail.com>
2014-09-02 21:51:34 -0400

    Python example of push node via synthesis of binaural beats

---
+++ bindings/python/examples/push.py
@@ -0,0 +1,17 @@
+import libaudioverse
+import math
+import time
+
+#build binaural beats: two sine waves slightly out of tune, one to the left channel and one to the right.
+l = [math.sin((i/44100.0)*2*math.pi*300) for i in xrange(44100)]
+r = [math.sin((i/44100.0)*2*math.pi*310) for i in xrange(44100)]
+stereo = [i for l in zip(l, r) for i in l]
+
+sim = libaudioverse.Simulation(device_index = -1)
+p = libaudioverse.PushObject(sim, 44100, 2)
+sim.output_object = p
+
+for i in xrange(5):
+	p.feed(len(stereo), stereo)
+
+time.sleep(6.0)
