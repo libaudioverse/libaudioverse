9ca0dd7fc0e7486f17b0866ff27bd4086cead0c6
Austin Hicks <camlorn38@gmail.com>
2014-09-06 19:17:51 -0400

    Modified the push example for testing purposes

--- bindings/python/examples/push.py
+++ bindings/python/examples/push.py
@@ -6,13 +6,19 @@ import time
 l = [math.sin((i/44100.0)*2*math.pi*300) for i in xrange(44100)]
 r = [math.sin((i/44100.0)*2*math.pi*310) for i in xrange(44100)]
 stereo = [i for l in zip(l, r) for i in l]
+segment = 2048
+index = 0
 
 sim = libaudioverse.Simulation(device_index = -1, block_size = 512, mix_ahead = 10)
 p = libaudioverse.PushObject(sim, 44100, 2)
 sim.output_object = p
 
 def feeder(obj):
-	obj.feed(len(stereo), stereo)
+	global segment, index
+	obj.feed(segment, stereo[index*segment:(index+1)*segment])
+	index += 1
+	if index*segment > len(stereo):
+		index = 0
 
 p.audio_callback = feeder
 time.sleep(10.0)
