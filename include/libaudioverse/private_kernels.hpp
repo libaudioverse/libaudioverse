aae968ac092e272a3f5d9548412deef9e4273fad
Austin Hicks <camlorn38@gmail.com>
2014-10-25 00:52:24 -0400

    Hopefully rewrote the interleaving/uninterleaving kernels to not fragment the heap

--- include/libaudioverse/private_kernels.hpp
+++ include/libaudioverse/private_kernels.hpp
@@ -3,6 +3,10 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #pragma once
 
+/**In-place interleaving and uninterleaving of samples.
+Note that these functions are actually the same; interleaveSamples simply calls uninterleaveSamples.  See interleaving.cpp for details of the algorithm.*/
+void uninterleaveSamples(unsigned int channels, unsigned int frames, float* samples);
+void interleaveSamples(unsigned int channels, unsigned int frames, float* samples);
 /**The convolution kernel.
 The first response-1 samples of the input buffer are assumed to be a running history, so the actual length of the input buffer needs to be outputSampleCount+responseLength-1.
 */
---
+++ src/libaudioverse/kernels/interleaving.cpp
@@ -0,0 +1,47 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+
+/**Knows how to take individual channels and combine them into an output buffer, or perform the inverse: separate a buffer of interleaved samples into individual buffers.*/
+#include <libaudioverse/private_kernels.hpp>
+#include <stdlib.h>
+#include <string.h>
+
+/**A brief note on the motivation:
+We can allocate a chunk of memory which is the same size as the original buffer, but this will cause rapid heap fragmentation.
+Alternatively, we can handle the samples in channels*channels matrices, i.e. channels frames of audio data at a time.
+This is still somewhat bad because there is no global buffer reuse.
+These are used seldomly in comparison with other kernels, and are unlikely to be a performance bottleneck.
+Note: this is technically the transpose of a square matrix, but we have to use 1-dimensional arrays which makes that much less clear.
+Todo: rewrrite this to use global buffers when such is implemented.
+*/
+
+void uninterleaveSamples(unsigned int channels, unsigned int frames, float* samples) {
+	float* tempBlock = new float[channels*channels];
+
+	//uninterleave them.
+	//this loop steps by samples, not frames.
+	//each iteration, i is at the next block's beginning.
+	for(unsigned int i = 0; i < frames*channels; i+= channels*channels) {
+		//fill the tempBlock.
+		for(unsigned int channel = 0; channel < channels; channel++) {
+			for(unsigned int frame= 0; frame< channels; frame++) {
+				//grab all the channel 1s, 2s, 3s, for example.
+				//the reverse order here makes us group channel 1s together, etc.
+				//yes, this is a complicated line.  There appears no simpler alternative.
+				tempBlock[channel*channels+frame] = i+frame*channels+channel >= frames*channels ? 0 : samples[i+frame*channels+channel];
+			}
+		}
+		//now, we splice it back in.
+		for(unsigned int j = 0; j < channels*channels; j++) {
+			if(i+j > frames*channels) break; //we hit the end of the input.
+			samples[i+j] = tempBlock[j];
+		}
+	}
+}
+
+/**Since the transpose of a transpose is the original matrix, we actually have that uninterleaving uninterleaved samples interleaves them. Realy.
+This function exists for readability.*/
+void interleaveSamples(unsigned int channels, unsigned int frames, float* samples) {
+	uninterleaveSamples(channels, frames, samples);
+}
