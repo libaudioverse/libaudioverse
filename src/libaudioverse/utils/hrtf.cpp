a4c518cd1d90e6fc6c2bcc184e9734d20310e895
Austin Hicks <camlorn38@gmail.com>
2014-11-07 21:38:13 -0500

    We used to normalize crossfaded coefficients, but this caused extreme artifacts

--- src/libaudioverse/utils/hrtf.cpp
+++ src/libaudioverse/utils/hrtf.cpp
@@ -216,13 +216,4 @@ void LavHrtfData::computeCoefficientsStereo(float elevation, float azimuth, floa
 	computeCoefficientsMono(elevation1, azimuth, temporary_buffer1);
 	computeCoefficientsMono(elevation2, azimuth, temporary_buffer2);
 	for(unsigned int i = 0; i < hrir_length; i++) left[i] = temporary_buffer1[i]*w1+temporary_buffer2[i]*w2;
-	//normalize in attempt to always produce no net decrease in volume.
-	auto cmp = [](const float x, const float y) {return fabs(x) < fabs(y);};
-	float lmax = *std::max_element<float*>(left, left+hrir_length, cmp);
-	float rmax = *std::max_element<float*>(right, right+hrir_length, cmp);
-	float maxVal = std::max(fabs(lmax), fabs(rmax));
-	for(unsigned int i = 0; i < hrir_length; i++) {
-		left[i]/=maxVal;
-		right[i]/=maxVal;
-	}
 }
