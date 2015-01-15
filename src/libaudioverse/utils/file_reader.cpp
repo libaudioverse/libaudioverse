4bc0486e10c2d994bb0ce110238c403288cadc34
Austin Hicks <camlorn38@gmail.com>
2015-01-15 11:25:06 -0500

    Fix all the utilities to compile

--- src/libaudioverse/utils/file_reader.cpp
+++ src/libaudioverse/utils/file_reader.cpp
@@ -1,10 +1,10 @@
 /**Copyright (C) Austin Hicks, 2014
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
-#include <libaudioverse/private_file.hpp>
+#include <libaudioverse/private/file.hpp>
 #include <libaudioverse/libaudioverse.h>
 #include <sndfile.h>
-#include <libaudioverse/private_errors.hpp>
+#include <libaudioverse/private/errors.hpp>
 
 LavFileReader::~LavFileReader() {
 	if(handle) close(); //make sure the file gets closed behind us.
