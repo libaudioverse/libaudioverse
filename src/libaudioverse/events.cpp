31f42c8a3375b1f808f245dee9161c4f9b7158a6
Austin Hicks <camlorn38@gmail.com>
2015-01-15 11:42:27 -0500

    Bring events back

--- src/libaudioverse/events.cpp
+++ src/libaudioverse/events.cpp
@@ -2,9 +2,9 @@
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #include <libaudioverse/libaudioverse.h>
-#include <libaudioverse/private_events.hpp>
-#include <libaudioverse/private_simulation.hpp>
-#include <libaudioverse/private_objects.hpp>
+#include <libaudioverse/private/events.hpp>
+#include <libaudioverse/private/simulation.hpp>
+#include <libaudioverse/private/node.hpp>
 #include <string>
 #include <memory>
 
@@ -15,7 +15,7 @@ LavEvent::LavEvent() {
 }
 
 LavEvent::LavEvent(const LavEvent &other):
-name(other.name), associated_simulation(other.associated_simulation), handler(other.handler), associated_object(other.associated_object), user_data(other.user_data), no_multifire(other.no_multifire) {
+name(other.name), associated_simulation(other.associated_simulation), handler(other.handler), associated_node(other.associated_node), user_data(other.user_data), no_multifire(other.no_multifire) {
 	is_firing.store(0);
 }
 
@@ -32,7 +32,7 @@ LavEventCallback LavEvent::getExternalHandler() {
 	return external_handler;
 }
 
-void LavEvent::setHandler(std::function<void(LavObject*, void*)> handler) {
+void LavEvent::setHandler(std::function<void(LavNode*, void*)> handler) {
 	this->handler = handler;
 }
 
@@ -40,15 +40,15 @@ void LavEvent::fire() {
 	if(handler == false) return; //nothing to do.
 	if(no_multifire && is_firing.load() == 1) return;
 	is_firing.store(1);
-	//we need to hold local copies of both the object and data in case they are changed between firing and processing by the simulation.
-	auto obj = std::weak_ptr<LavObject>(associated_object->shared_from_this());
+	//we need to hold local copies of both the node and data in case they are changed between firing and processing by the simulation.
+	auto node= std::weak_ptr<LavNode>(associated_node->shared_from_this());
 	void* userdata = user_data;
 	//fire a lambda that uses these by copy.
 	associated_simulation->enqueueTask([=]() {
-		auto shared_obj = obj.lock();
-		if(shared_obj == nullptr) return;	
-		//callbacks die with objects; if we get this far, this is still a valid pointer.
-		handler(shared_obj.get(), userdata);
+		auto shared_node= node.lock();
+		if(shared_node== nullptr) return;	
+		//callbacks die with nodes; if we get this far, this is still a valid pointer.
+		handler(shared_node.get(), userdata);
 		this->is_firing.store(0);
 	});
 }
@@ -57,8 +57,8 @@ void LavEvent::associateSimulation(std::shared_ptr<LavSimulation> simulation) {
 	associated_simulation = simulation;
 }
 
-void LavEvent::associateObject(LavObject* obj) {
-	associated_object = obj;
+void LavEvent::associateNode(LavNode* node) {
+	associated_node= node;
 }
 
 const char* LavEvent::getName() {
