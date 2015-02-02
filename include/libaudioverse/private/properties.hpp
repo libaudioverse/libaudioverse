9a0157359a2812c9206dfbc14f3f2a40d1771903
Austin Hicks <camlorn38@gmail.com>
2015-02-02 13:10:35 -0500

    Move all of the code for properties out of  the header

--- include/libaudioverse/private/properties.hpp
+++ include/libaudioverse/private/properties.hpp
@@ -26,165 +26,93 @@ class LavProperty {
 	public:
 	LavProperty() = default;
 	LavProperty(const LavProperty&) = default;
-	explicit LavProperty(int property_type): type(property_type) {}
-	void reset() {value = default_value; string_value = default_string_value; farray_value = default_farray_value; iarray_value = default_iarray_value;}
-	int getType() { return type;}
-	void setType(int t) {type = t;}
-	int isType(int t) { return type == t;} //we have to typecheck these everywhere.
-	const char* getName() {return name.c_str();}
-	void setName(const char* n) { name = std::string(n);}
-	int getTag() {return tag;}
-	void setTag(int t) {tag = t;}
-	bool isReadOnly() { return read_only;}
-	void setReadOnly(bool what) {read_only = what;}
+	explicit LavProperty(int property_type);
+	void reset();
+	int getType();
+	void setType(int t);
+	int isType(int t);
+	const char* getName();
+	void setName(const char* n);
+	int getTag();
+	void setTag(int t);
+	bool isReadOnly();
+	void setReadOnly(bool what);
 
 	//yes, really. This is as uggly as it looks.
-	int getIntValue() { return value.ival;}
-	void setIntValue(int v) {
-		RC(v, ival);
-		value.ival = v;
-		if(post_changed_callback) post_changed_callback();
-	}	
-	int getIntDefault() {return default_value.ival;}
-	void setIntDefault(int d) {default_value.ival = d;}
-	int getIntMin() {return minimum_value.ival;}
-	int getIntMax() {return maximum_value.ival;}
-	void setIntRange(int a, int b) {minimum_value.ival = a; maximum_value.ival = b;}
+	int getIntValue();
+	void setIntValue(int v);
+	int getIntDefault();
+	void setIntDefault(int d);
+	int getIntMin();
+	int getIntMax();
+	void setIntRange(int a, int b);
 
 	//floats...
-	float getFloatValue() {return value.fval;}
-	void setFloatValue(float v) {
-	RC(v, fval);
-		value.fval = v;
-		if(post_changed_callback) post_changed_callback();
-	}
-	float getFloatDefault() {return default_value.fval;}
-	void setFloatDefault(float v) {default_value.fval = v;}
-	float getFloatMin() {return minimum_value.fval;}
-	float getFloatMax() {return maximum_value.fval;}
-	void setFloatRange(float a, float b) {minimum_value.fval = a; maximum_value.fval = b;}
+	float getFloatValue();
+	void setFloatValue(float v);
+	float getFloatDefault();
+	void setFloatDefault(float v);
+	float getFloatMin();
+	float getFloatMax();
+	void setFloatRange(float a, float b);
 
 	//doubles...
-	double getDoubleValue() {return value.dval;}
-	void setDoubleValue(double v) {
-		RC(v, dval);
-		value.dval = v;
-		if(post_changed_callback) post_changed_callback();
-	}
-	double getDoubleMin() {return minimum_value.dval;}
-	double getDoubleMax() {return maximum_value.dval;}
-	void setDoubleRange(double a, double b) {minimum_value.dval = a; maximum_value.dval = b;}
-	void setDoubleDefault(double v) {default_value.dval = v;}
+	double getDoubleValue();
+	void setDoubleValue(double v);
+	double getDoubleMin();
+	double getDoubleMax();
+	void setDoubleRange(double a, double b);
+	void setDoubleDefault(double v);
 
 	//float3 vectors.
-	const float* getFloat3Value() {return value.f3val;}
-	const float* getFloat3Default() {return default_value.f3val;}
-	void setFloat3Value(const float* const v) {
-		memcpy(value.f3val, v, sizeof(float)*3);
-		if(post_changed_callback) post_changed_callback();
-}
-	void setFloat3Value(float v1, float v2, float v3) {
-		value.f3val[0] = v1; value.f3val[1] = v2; value.f3val[2] = v3;
-		if(post_changed_callback) post_changed_callback();
-	}
-	void setFloat3Default(const float* const v) {memcpy(default_value.f3val, v, sizeof(float)*3);}
-	void setFloat3Default(float v1, float v2, float v3) {default_value.f3val[0] = v1; default_value.f3val[1] = v2; default_value.f3val[2] = v3;}
+	const float* getFloat3Value();
+	const float* getFloat3Default();
+	void setFloat3Value(const float* const v);
+	void setFloat3Value(float v1, float v2, float v3);
+	void setFloat3Default(const float* const v);
+	void setFloat3Default(float v1, float v2, float v3);
 
 	//float6 vectors.
-	const float* getFloat6Value() {return value.f6val;}
-	const float* getFloat6Default() {return default_value.f6val;}
-	void setFloat6Value(const float* const v) {
-		memcpy(&value.f6val, v, sizeof(float)*6);
-		if(post_changed_callback) post_changed_callback();
-	}
-	void setFloat6Value(float v1, float v2, float v3, float v4, float v5, float v6) {
-		value.f6val[0] = v1; value.f6val[1] = v2; value.f6val[2] = v3; value.f6val[3] = v4; value.f6val[4] = v5; value.f6val[5] = v6;
-		if(post_changed_callback) post_changed_callback();
-	}
-	void setFloat6Default(const float* const v) {memcpy(default_value.f6val, v, sizeof(float)*6);}
-	void setFloat6Default(float v1, float v2, float v3, float v4, float v5, float v6) {default_value.f6val[0] = v1; default_value.f6val[1] = v2; default_value.f6val[2] = v3; default_value.f6val[3] = v4; default_value.f6val[4] = v5; default_value.f6val[5] = v6;}
+	const float* getFloat6Value();
+	const float* getFloat6Default();
+	void setFloat6Value(const float* const v);
+	void setFloat6Value(float v1, float v2, float v3, float v4, float v5, float v6);
+	void setFloat6Default(const float* const v);
+	void setFloat6Default(float v1, float v2, float v3, float v4, float v5, float v6);
 
 	//applies to both array properties.
 	//note that in the below, we again disable all range checks for properties which are read_only.
-	void setArrayLengthRange(unsigned int lower, unsigned int upper) { min_array_length = lower; max_array_length = upper;}
-	void getArraylengthRange(unsigned int* min, unsigned int* max) {*min = min_array_length; *max = max_array_length;}
+	void setArrayLengthRange(unsigned int lower, unsigned int upper);
+	void getArraylengthRange(unsigned int* min, unsigned int* max);
 
 	//the float arrays.
-	float readFloatArray(unsigned int index) {
-		if(index >= farray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
-		return farray_value[index];
-	}
-	void writeFloatArray(unsigned int start, unsigned int stop, float* values) {
-		if(start >= farray_value.size() || stop > farray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
-		for(unsigned int i = start; i < stop; i++) {
-			farray_value[start] = values[i];
-		}
-		if(post_changed_callback) post_changed_callback();
-	}
-	void replaceFloatArray(unsigned int length, float* values) {
-		if((length < min_array_length || length > max_array_length) && read_only == false) throw LavErrorException(Lav_ERROR_RANGE);
-		farray_value.resize(length);
-		std::copy(values, values+length, farray_value.begin());
-		if(post_changed_callback) post_changed_callback();
-	}
-	unsigned int getFloatArrayLength() {
-		return farray_value.size();
-	}
-	std::vector<float> getFloatArrayDefault() {
-		return default_farray_value;
-	}
-	void setFloatArrayDefault(std::vector<float> d) {
-		default_farray_value = d;
-	}
+	float readFloatArray(unsigned int index);
+	void writeFloatArray(unsigned int start, unsigned int stop, float* values);
+	void replaceFloatArray(unsigned int length, float* values);
+	unsigned int getFloatArrayLength();
+	std::vector<float> getFloatArrayDefault();
+	void setFloatArrayDefault(std::vector<float> d);
 	
 	//the int arrays.
-	int readIntArray(unsigned int index) {
-		if(index >= iarray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
-		return iarray_value[index];
-	}
-	void writeIntArray(unsigned int start, unsigned int stop, int* values) {
-		if(start >= iarray_value.size() || stop > iarray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
-		for(unsigned int i = start; i < stop; i++) {
-			iarray_value[start] = values[i];
-		}
-		if(post_changed_callback) post_changed_callback();
-	}
-	void replaceIntArray(unsigned int length, int* values) {
-		if((length < min_array_length || length > max_array_length) && read_only == false) throw LavErrorException(Lav_ERROR_RANGE);
-		iarray_value.resize(length);
-		std::copy(values, values+length, iarray_value.begin());
-		if(post_changed_callback) post_changed_callback();
-	}
-	unsigned int getIntArrayLength() {
-		return iarray_value.size();
-	}
-	std::vector<int> getIntArrayDefault() {
-		return default_iarray_value;
-	}
-	void setIntArrayDefault(std::vector<int> d) {
-		default_iarray_value = d;
-	}
+	int readIntArray(unsigned int index);
+	void writeIntArray(unsigned int start, unsigned int stop, int* values);
+	void replaceIntArray(unsigned int length, int* values);
+	unsigned int getIntArrayLength();
+	std::vector<int> getIntArrayDefault();
+	void setIntArrayDefault(std::vector<int> d);
 
 	//strings:
-	const char* getStringValue() { return string_value.c_str();}
-	void setStringValue(const char* s) {
-		string_value = s;
-		if(post_changed_callback) post_changed_callback();
-	}
-	const char* getStringDefault() { return default_string_value.c_str();}
-	void setStringDefault(const char* s) { default_string_value = s;}
+	const char* getStringValue();
+	void setStringValue(const char* s);
+	const char* getStringDefault();
+	void setStringDefault(const char* s);
 
 	//set the callback...
-	void setPostChangedCallback(std::function<void(void)> cb) {post_changed_callback = cb;}
+	void setPostChangedCallback(std::function<void(void)> cb);
 
 	//get/set dynamic range status.
-	bool getHasDynamicRange() {
-		return has_dynamic_range;
-	}
-
-	void setHasDynamicRange(bool v) {
-		has_dynamic_range = v;
-	}
+	bool getHasDynamicRange();
+	void setHasDynamicRange(bool v);
 
 	private:
 	int type, tag;
--- src/libaudioverse/properties.cpp
+++ src/libaudioverse/properties.cpp
@@ -6,7 +6,312 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #include <stdlib.h>
 #include <string.h>
 
-/*Note: having properties be very very very fast is important, so consequently go see the header file.*/
+LavProperty::LavProperty(int property_type): type(property_type) {}
+
+void LavProperty::reset() {
+	value = default_value;
+	string_value = default_string_value;
+	farray_value = default_farray_value;
+	iarray_value = default_iarray_value;
+}
+
+int LavProperty::getType() {
+	return type;
+}
+
+void LavProperty::setType(int t) {
+	type = t;
+}
+
+int LavProperty::isType(int t) { 
+	return type == t;
+}
+
+const char* LavProperty::getName() {
+	return name.c_str();
+}
+
+void LavProperty::setName(const char* n) {
+	name = std::string(n);
+}
+
+int LavProperty::getTag() {
+	return tag;
+}
+
+void LavProperty::setTag(int t) {
+	tag = t;
+}
+
+bool LavProperty::isReadOnly() {
+	return read_only;
+}
+
+void LavProperty::setReadOnly(bool what) {
+	read_only = what;
+}
+
+int LavProperty::getIntValue() {
+	return value.ival;
+}
+
+void LavProperty::setIntValue(int v) {
+	RC(v, ival);
+	value.ival = v;
+	if(post_changed_callback) post_changed_callback();
+}	
+
+int LavProperty::getIntDefault() {
+	return default_value.ival;
+}
+
+void LavProperty::setIntDefault(int d) {
+	default_value.ival = d;
+}
+
+int LavProperty::getIntMin() {
+	return minimum_value.ival;
+}
+
+int LavProperty::getIntMax() {
+	return maximum_value.ival;
+}
+
+void LavProperty::setIntRange(int a, int b) {
+	minimum_value.ival = a;
+	maximum_value.ival = b;
+}
+
+
+float LavProperty::getFloatValue() {
+	return value.fval;
+}
+
+void LavProperty::setFloatValue(float v) {
+	RC(v, fval);
+	value.fval = v;
+	if(post_changed_callback) post_changed_callback();
+}
+
+float LavProperty::getFloatDefault() {
+	return default_value.fval;
+}
+
+void LavProperty::setFloatDefault(float v) {
+	default_value.fval = v;
+}
+
+float LavProperty::getFloatMin() {
+	return minimum_value.fval;
+}
+
+float LavProperty::getFloatMax() {
+	return maximum_value.fval;
+}
+
+void LavProperty::setFloatRange(float a, float b) {
+	minimum_value.fval = a; maximum_value.fval = b;
+}
+
+//doubles...
+double LavProperty::getDoubleValue() {
+	return value.dval;
+}
+
+void LavProperty::setDoubleValue(double v) {
+	RC(v, dval);
+	value.dval = v;
+	if(post_changed_callback) post_changed_callback();
+}
+
+double LavProperty::getDoubleMin() {
+	return minimum_value.dval;
+}
+
+double LavProperty::getDoubleMax() {
+	return maximum_value.dval;
+}
+
+void LavProperty::setDoubleRange(double a, double b) {
+	minimum_value.dval = a;
+	maximum_value.dval = b;
+}
+
+void LavProperty::setDoubleDefault(double v) {
+	default_value.dval = v;
+}
+
+const float* LavProperty::getFloat3Value() {
+	return value.f3val;
+}
+
+const float* LavProperty::getFloat3Default() {
+	return default_value.f3val;
+}
+
+void LavProperty::setFloat3Value(const float* const v) {
+	memcpy(value.f3val, v, sizeof(float)*3);
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::setFloat3Value(float v1, float v2, float v3) {
+	value.f3val[0] = v1;
+	value.f3val[1] = v2;
+	value.f3val[2] = v3;
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::setFloat3Default(const float* const v) {
+	memcpy(default_value.f3val, v, sizeof(float)*3);
+}
+
+void LavProperty::setFloat3Default(float v1, float v2, float v3) {
+	default_value.f3val[0] = v1;
+	default_value.f3val[1] = v2;
+	default_value.f3val[2] = v3;
+}
+
+const float* LavProperty::getFloat6Value() {
+	return value.f6val;
+}
+
+const float* LavProperty::getFloat6Default() {
+	return default_value.f6val;
+}
+
+void LavProperty::setFloat6Value(const float* const v) {
+	memcpy(&value.f6val, v, sizeof(float)*6);
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::setFloat6Value(float v1, float v2, float v3, float v4, float v5, float v6) {
+	value.f6val[0] = v1;
+	value.f6val[1] = v2;
+	value.f6val[2] = v3;
+	value.f6val[3] = v4;
+	value.f6val[4] = v5;
+	value.f6val[5] = v6;
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::setFloat6Default(const float* const v) {
+	memcpy(default_value.f6val, v, sizeof(float)*6);
+}
+
+void LavProperty::setFloat6Default(float v1, float v2, float v3, float v4, float v5, float v6) {
+	default_value.f6val[0] = v1;
+	default_value.f6val[1] = v2;
+	default_value.f6val[2] = v3;
+	default_value.f6val[3] = v4;
+	default_value.f6val[4] = v5;
+	default_value.f6val[5] = v6;
+}
+
+void LavProperty::setArrayLengthRange(unsigned int lower, unsigned int upper) {
+	min_array_length = lower;
+	max_array_length = upper;
+}
+
+void LavProperty::getArraylengthRange(unsigned int* min, unsigned int* max) {
+	*min = min_array_length;
+	*max = max_array_length;
+}
+
+float LavProperty::readFloatArray(unsigned int index) {
+	if(index >= farray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
+	return farray_value[index];
+}
+
+void LavProperty::writeFloatArray(unsigned int start, unsigned int stop, float* values) {
+	if(start >= farray_value.size() || stop > farray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
+	for(unsigned int i = start; i < stop; i++) {
+		farray_value[start] = values[i];
+	}
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::replaceFloatArray(unsigned int length, float* values) {
+	if((length < min_array_length || length > max_array_length) && read_only == false) throw LavErrorException(Lav_ERROR_RANGE);
+	farray_value.resize(length);
+	std::copy(values, values+length, farray_value.begin());
+	if(post_changed_callback) post_changed_callback();
+}
+
+unsigned int LavProperty::getFloatArrayLength() {
+	return farray_value.size();
+}
+
+std::vector<float> LavProperty::getFloatArrayDefault() {
+	return default_farray_value;
+}
+
+void LavProperty::setFloatArrayDefault(std::vector<float> d) {
+	default_farray_value = d;
+}
+
+int LavProperty::readIntArray(unsigned int index) {
+	if(index >= iarray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
+	return iarray_value[index];
+}
+
+void LavProperty::writeIntArray(unsigned int start, unsigned int stop, int* values) {
+	if(start >= iarray_value.size() || stop > iarray_value.size()) throw LavErrorException(Lav_ERROR_RANGE);
+	for(unsigned int i = start; i < stop; i++) {
+		iarray_value[start] = values[i];
+	}
+	if(post_changed_callback) post_changed_callback();
+}
+
+void LavProperty::replaceIntArray(unsigned int length, int* values) {
+	if((length < min_array_length || length > max_array_length) && read_only == false) throw LavErrorException(Lav_ERROR_RANGE);
+	iarray_value.resize(length);
+	std::copy(values, values+length, iarray_value.begin());
+	if(post_changed_callback) post_changed_callback();
+}
+
+unsigned int LavProperty::getIntArrayLength() {
+	return iarray_value.size();
+}
+
+std::vector<int> LavProperty::getIntArrayDefault() {
+	return default_iarray_value;
+}
+
+void LavProperty::setIntArrayDefault(std::vector<int> d) {
+	default_iarray_value = d;
+}
+
+const char* LavProperty::getStringValue() {
+	return string_value.c_str();
+}
+
+void LavProperty::setStringValue(const char* s) {
+	string_value = s;
+	if(post_changed_callback) post_changed_callback();
+}
+
+const char* LavProperty::getStringDefault() {
+	return default_string_value.c_str();
+}
+
+void LavProperty::setStringDefault(const char* s) {
+	default_string_value = s;
+}
+
+void LavProperty::setPostChangedCallback(std::function<void(void)> cb) {
+	post_changed_callback = cb;
+}
+
+bool LavProperty::getHasDynamicRange() {
+	return has_dynamic_range;
+}
+
+void LavProperty::setHasDynamicRange(bool v) {
+	has_dynamic_range = v;
+}
+
+//Property creators.
+
 
 LavProperty* createIntProperty(const char* name, int default, int min, int max) {
 	LavProperty* retval = new LavProperty(Lav_PROPERTYTYPE_INT);
