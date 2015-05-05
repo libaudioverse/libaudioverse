891633e32297b6bfe9ca2ebd672ed50402232918
Austin Hicks <camlorn38@gmail.com>
2015-05-05 17:44:44 -0400

    Massively modularize the bindings generator

--- bindings/metadata_handler.py
+++ bindings/metadata_handler.py
@@ -4,24 +4,27 @@ import glob
 import os
 import os.path
 
-#Compute where the metadata is so we can load it.
-metadata_dir = os.path.abspath(os.path.join(
-os.path.split(os.path.abspath(__file__))[0],
-"..",
-"metadata"
-))
+def make_metadata():
+	#Compute where the metadata is so we can load it.
+	metadata_dir = os.path.abspath(os.path.join(
+	os.path.split(os.path.abspath(__file__))[0],
+	"..",
+	"metadata"
+	))
 
-#First, build the base dict. This is metada.yaml:
-with file(os.path.join(metadata_dir, "metadata.y")) as f:
-	metadata = yaml.load(f)
+	#First, build the base dict. This is metada.yaml:
+	with file(os.path.join(metadata_dir, "metadata.y")) as f:
+		metadata = yaml.load(f)
 
-#Next, we add all the files in the nodes subdirectory:
-nodes = glob.glob(os.path.join(metadata_dir, "nodes", "*.y"))
-nodes= [os.path.abspath(i) for i in nodes]
-metadata['nodes'] = dict()
+	#Next, we add all the files in the nodes subdirectory:
+	nodes = glob.glob(os.path.join(metadata_dir, "nodes", "*.y"))
+	nodes= [os.path.abspath(i) for i in nodes]
+	metadata['nodes'] = dict()
 
-for i in nodes:
-	with file(i) as f:
-		node_info = yaml.load(f)
-	name =os.path.split(i)[1][:-len(".y")]
-	metadata['nodes'][name] = node_info
+	for i in nodes:
+		with file(i) as f:
+			node_info = yaml.load(f)
+		name =os.path.split(i)[1][:-len(".y")]
+		metadata['nodes'][name] = node_info
+
+	return metadata
