b6f0d9673b04fb95bfe5ecb2a23d57228a7178c7
Austin Hicks <camlorn38@gmail.com>
2015-05-14 15:45:03 -0400

    Revert "Optimized sine nodes to buy somewhere between 100 and 150 more in realtime on x86."
    This reverts commit 42e296c71e4c4482116ed7f880148ddc8a4f3239.

--- src/libaudioverse/nodes/sine.cpp
+++ src/libaudioverse/nodes/sine.cpp
@@ -23,7 +23,7 @@ class SineNode: public Node {
 	SineNode(std::shared_ptr<Simulation> simulation);
 	virtual void process();
 	virtual void reset() override;
-	float phase = 0;
+	double phase = 0;
 };
 
 SineNode::SineNode(std::shared_ptr<Simulation> simulation): Node(Lav_OBJTYPE_SINE_NODE, simulation, 0, 1) {
@@ -41,10 +41,9 @@ void SineNode::process() {
 	if(freqProp.needsARate()==false) {
 		double phaseDelta=freqProp.getFloatValue()/simulation->getSr();
 		for(unsigned int i = 0; i< block_size; i++) {
-			///the (float) in the next line is important to keep us from promoting to double.
-			output_buffers[0][i] = sinf(2*phase*(float)PI);
+			output_buffers[0][i] = (float)sin(2*phase*PI);
+			phase+=phaseDelta;
 		}
-		phase+= block_size*phaseDelta;
 	}
 	else {
 		for(unsigned int i = 0; i< block_size; i++) {
