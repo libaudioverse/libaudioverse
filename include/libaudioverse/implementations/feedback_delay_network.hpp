aaccccd7a69899804e7b3326f9507deccf9e160e
Austin Hicks <camlorn38@gmail.com>
2015-06-13 18:21:49 -0400

    The internal implementation of the feedback delay network matches JOS

--- include/libaudioverse/implementations/feedback_delay_network.hpp
+++ include/libaudioverse/implementations/feedback_delay_network.hpp
@@ -4,47 +4,35 @@ A copy of the GPL, as well as other important copyright and licensing informatio
 #pragma once
 
 namespace libaudioverse_implementation {
-
 /**A feedback delay network consists of the following:
 N delay lines of some quality or type.
 A vector of length n representing relative output gains.
 An n*n matrix of feedbacks.
-An n*n matrix of feedback delays.
 
-This last is nonstandard, but one common use case for this system is representing reverb and waveguide networks.
-In such systems, it is needed to simulate sound waves travelling outward from a point, which can be thought of as two things:
-- A feedback value representing attenuation.
-- A feedback writing position representing the time it took to arrive.
-Until the first time this matrix is set, it is not used. Once code sets this matrix, it is the responsibility of that code to manage it, not us.
+To advance this object, call computeFrame, add your inputs to the computed values, then call advance.
+Many nodes will wish to customize this process via the insertion of filters, so it is left to those nodes to carry out this step.
 
-Unlike normal delay lines, the advance funciton requires both an input frame and the last output frame.
-This allows the last output frame to be modified before feeding it back in.
+This matches the formulation given in Physical Audio Processing for Virtual Musical Instruments and Audio Effects by julius O. Smith III.
 */
-
 
 class FeedbackDelayNetwork {
 	public:
 	FeedbackDelayNetwork(int n, float maxDelay, float sr);
 	~FeedbackDelayNetwork();
 	void computeFrame(float* outputs);
-	void advance(float* nextInput, float* lastOutput);
-	void setFeedbackMatrix(float* feedbacks);
-	void setFeedbackDelayMatrix(float* feedbackDelays);
+	void advance(float* inputs);
+	void setMatrix(float* feedbacks);
 	void setDelays(float* delays);
-	void setCrossfadingTime(float time);
+	void setDelayCrossfadingTime(float time);
 	void reset();
 	private:
-	//fills first row of workspace with a frame of feedbacks for bank.
-	void computeFeedbacks(float* lastInput);
-	void advanceNoFeedbackDelayMatrix(float* lastInput, float* nextOutput);
-	void advanceFeedbackDelayMatrix(float* nextInput, float* lastOutput);
 	int n;
 	float sr;
-	CrossfadingDelayLine **bank = nullptr;
-	float *feedback_matrix = nullptr, *feedback_delay_matrix = nullptr;
-	//avoids very continuous reallocation, an n*n matrix.
+	CrossfadingDelayLine **lines = nullptr;
+	float *matrix = nullptr;
+	//avoids very continuous reallocation, an n*n matrix that can be used for any purpose.
 	float* workspace = nullptr;
 	float* delays = nullptr;
-	bool using_feedback_delay_matrix = false;
 };
+
 }
--- src/libaudioverse/implementations/feedback_delay_network.cpp
+++ src/libaudioverse/implementations/feedback_delay_network.cpp
@@ -13,98 +13,53 @@ namespace libaudioverse_implementation {
 FeedbackDelayNetwork::FeedbackDelayNetwork(int n, float maxDelay, float sr) {
 	this->n = n;
 	this->sr = sr;
-	this->using_feedback_delay_matrix = false;
-	bank = new CrossfadingDelayLine*[n];
-	for(int i = 0; i < n; i++) bank[i] = new CrossfadingDelayLine(maxDelay, sr);
-	feedback_matrix = new float[n*n]();
-	feedback_delay_matrix = new float[n*n]();
+	lines = new CrossfadingDelayLine*[n];
+	for(int i = 0; i < n; i++) lines[i] = new CrossfadingDelayLine(maxDelay, sr);
+	matrix = new float[n*n]();
 	delays = new float[n]();
 	workspace = new float[n*n]();
 }
 
 FeedbackDelayNetwork::~FeedbackDelayNetwork() {
-	for(int i = 0; i < n; i++) delete bank[i];
-	delete[] bank;
-	delete[] feedback_matrix;
-	delete[] feedback_delay_matrix;
+	for(int i = 0; i < n; i++) delete lines[i];
+	delete[] lines;
+	delete[] matrix;
 	delete[] delays;
 	delete[] workspace;
 }
 
 void FeedbackDelayNetwork::computeFrame(float* outputs) {
-	//the output step is not special.
-	for(int i = 0; i < n; i++) outputs[i] = bank[i]->computeSample();
-}
-
-void FeedbackDelayNetwork::advance(float* nextInput, float* lastOutput) {
-	//two cases. The first is that we're not using the feedback delay matrix.
-	if(using_feedback_delay_matrix == false) {
-		advanceNoFeedbackDelayMatrix(nextInput, lastOutput);
-	} else {
-		advanceFeedbackDelayMatrix(nextInput, lastOutput);
+	//This is a matrix multiplication, but arranged to avoid reading lines repeatedly.
+	memset(outputs, 0, n*sizeof(float));
+	//I is column, j is row.
+	for(int i = 0; i < n; i++) {
+		float sample = lines[i]->computeSample();
+		for(int j = 0; j < n; j++) outputs[j] += matrix[j*n+i]*sample;
 	}
 }
 
-void FeedbackDelayNetwork::computeFeedbacks(float* lastOutput) {
-	for(int row = 0; row < n; row++) {
-		for(int column = 0; column < n; column++) {
-			workspace[row*n+column] = lastOutput[row]*feedback_matrix[row*n+column];
-		}
-	}
-	//now we sum them in the first row.
-	for(int column = 0; column< n; column++) {
-		for(int row = 1; row < n; row++) {
-			workspace[column] += workspace[row*n+column];
-		}
-	}
-	//and now the first row of workspace is a frame of feedback.
-}
-
-void FeedbackDelayNetwork::advanceNoFeedbackDelayMatrix(float* nextInput, float* lastOutput) {
-	//in this case, we advance the delay lines with their computed feedback samples.
-	//fill workspace with a computed feedback frame:
-	computeFeedbacks(lastOutput);
-	//sum onto workspace the next input frame.
-	for(int column = 0; column < n; column++) workspace[column]+=nextInput[column];
-	//and feed this to the bank.
-	for(int column = 0; column < n; column++) bank[column]->advance(workspace[column]);
-}
-
-void FeedbackDelayNetwork::advanceFeedbackDelayMatrix(float* nextInput, float* lastOutput) {
-	//in this case, we do the same as above.
-	//but instead of advancing with feedback+input, we advance with input and then manually write the feedbacks.
-	for(int column = 0; column < n; column++) bank[column]->advance(nextInput[column]);
-	//fills workspace as before.
-	computeFeedbacks(lastOutput);
-	//now, we write.
-	for(int column = 0; column < n; column++) {
-		for(int row = 0; row < n; row++) {
-			bank[column]->add(feedback_delay_matrix[column*n+row], workspace[column]);
-		}
-	}
+void FeedbackDelayNetwork::advance(float* inputs) {
+	//Just write all the delay lines.
+	for(int i=0; i < n; i++) lines[i]->advance(inputs[i]);
 }
 
 //below here is not very algorithmic, just setters.
 
-void FeedbackDelayNetwork::setFeedbackMatrix(float* feedbacks) {
-	std::copy(feedbacks, feedbacks+n*n, feedback_matrix);
-}
-
-void FeedbackDelayNetwork::setFeedbackDelayMatrix(float* feedbackDelays) {
-	std::copy(feedbackDelays, feedbackDelays+n*n, feedback_delay_matrix);
+void FeedbackDelayNetwork::setMatrix(float* feedbacks) {
+	std::copy(feedbacks, feedbacks+n*n, matrix);
 }
 
 void FeedbackDelayNetwork::setDelays(float* delays) {
 	std::copy(delays, delays+n, this->delays);
-	for(int i = 0; i < n; i++) bank[i]->setDelay(delays[i]);
+	for(int i = 0; i < n; i++) lines[i]->setDelay(delays[i]);
 }
 
-void FeedbackDelayNetwork::setCrossfadingTime(float time) {
-	for(int i = 0; i < n; i++) bank[i]->setInterpolationTime(time);
+void FeedbackDelayNetwork::setDelayCrossfadingTime(float time) {
+	for(int i = 0; i < n; i++) lines[i]->setInterpolationTime(time);
 }
 
 void FeedbackDelayNetwork::reset() {
-	for(int i = 0; i < n; i++) bank[i]->reset();
+	for(int i = 0; i < n; i++) lines[i]->reset();
 }
 
 }
