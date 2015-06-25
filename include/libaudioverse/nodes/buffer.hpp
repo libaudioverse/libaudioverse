ca43e16bd4291de84ea772f112dade8a8a1292ae
Austin Hicks <camlorn38@gmail.com>
2015-06-25 15:18:46 -0400

    Add a missing file.

---
+++ include/libaudioverse/nodes/buffer.hpp
@@ -0,0 +1,22 @@
+/**Copyright (C) Austin Hicks, 2014
+This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
+A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
+#pragma once
+#include "../private/node.hpp"
+#include "../implementations/feedback_delay_network.hpp"
+#include <memory>
+
+namespace libaudioverse_implementation {
+
+class BufferNode: public Node {
+	public:
+	BufferNode(std::shared_ptr<Simulation> simulation);
+	void setBuffer(std::shared_ptr<Buffer> buff);
+	virtual void positionChanged();
+	virtual void process();
+	int frame = 0;
+	int buffer_length=0;
+	double offset=0.0;
+};
+
+}
