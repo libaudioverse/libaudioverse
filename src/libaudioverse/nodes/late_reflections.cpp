d75edac8cf25773502683aa18cbd57fc0a2718b4
Austin Hicks <camlorn38@gmail.com>
2015-06-27 12:31:59 -0400

    Tweak the density to delay conversion.

--- src/libaudioverse/nodes/late_reflections.cpp
+++ src/libaudioverse/nodes/late_reflections.cpp
@@ -136,7 +136,7 @@ void LateReflectionsNode::recompute() {
 	float hf_reference=getProperty(Lav_LATE_REFLECTIONS_HF_REFERENCE).getFloatValue();
 	float lf_reference = getProperty(Lav_LATE_REFLECTIONS_LF_REFERENCE).getFloatValue();
 	//The base delay is the amount we are delaying all delay lines by.
-	float baseDelay = 0.01+(1.0f-density)*0.03;
+	float baseDelay = 0.003+(1.0f-density)*0.03;
 	//Approximate delay line lengths using powers of primes.
 	int step =order/16;
 	for(int i = 0; i < 16; i+=1) {
