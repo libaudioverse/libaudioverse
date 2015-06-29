f805779535c9dbc78d7fde88d148bcc722df1980
Austin Hicks <camlorn38@gmail.com>
2015-06-28 20:42:34 -0400

    Move back to the hadamard, temporarily.

--- src/libaudioverse/nodes/late_reflections.cpp
+++ src/libaudioverse/nodes/late_reflections.cpp
@@ -1,4 +1,4 @@
-/**Copyright (C) Austin Hicks, 2014
+/**Copyright (C) Austin Hicks, 2014
 This file is part of Libaudioverse, a library for 3D and environmental audio simulation, and is released under the terms of the Gnu General Public License Version 3 or (at your option) any later version.
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 
@@ -84,8 +84,8 @@ fdn(order, 1.0f, simulation->getSr()) {
 		appendOutputConnection(i, 1);
 	}
 	fdn_matrix=allocArray<float>(order*order);
-	//get a householder.
-	householder(order, fdn_matrix);
+	//get a hadamard.
+	hadamard(order, fdn_matrix);
 	//feed the fdn the initial matrix.
 	fdn.setMatrix(fdn_matrix);
 	//this is fixed...for now.
