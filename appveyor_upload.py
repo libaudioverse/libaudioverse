4c6fd93fc173c95130c33a33a4a3659266a29193
Austin Hicks <camlorn38@gmail.com>
2015-10-05 17:29:36 -0400

    Support uploading to camlorn.net

---
+++ appveyor_upload.py
@@ -0,0 +1,28 @@
+import paramiko
+import os
+import os.path
+
+username = os.getenv("DEPLOYMENT_USERNAME")
+password = os.getenv("DEPLOYMENT_PASSWORD")
+host = os.getenv("DEPLOYMENT_HOST")
+port = 22
+
+if host is None:
+    print "Host?"
+    host = raw_input()
+if username is None:
+    print "Username?"
+    username = raw_input()
+if password is None:
+    print "Password?"
+    password = raw_input()
+
+transport=paramiko.Transport(host, 22)
+transport.connect(username = username, password = password)
+with paramiko.SFTPClient.from_transport(transport) as sftp:
+    script_dir = os.path.split(os.path.abspath(__file__))[0]
+    build_dir = os.path.join(script_dir, "build")
+    sftp.chdir("releases/libaudioverse")
+    sftp.put(
+    os.path.join(build_dir, "libaudioverse_master.zip"),
+    "libaudioverse_master.zip")
