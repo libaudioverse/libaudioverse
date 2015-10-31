cc6d05a845ffdf39e3de7c0fafd6cf5f137b601c
Austin Hicks <camlorn38@gmail.com>
2015-10-30 21:34:47 -0400

    Bunch of small fixes to the BLIT that make it usable.

--- include/libaudioverse/implementations/blit.hpp
+++ include/libaudioverse/implementations/blit.hpp
@@ -34,14 +34,19 @@ inline Blit::Blit(float _sr): sr(_sr) {
 }
 
 inline float Blit::tick() {
-	double numer = std::sin(phase*(adjusted_harmonics+1)/2.0);
-	double denom = std::sin(phase/2);
+	double numer = std::sin(phase*(adjusted_harmonics+0.5));
+	double denom = std::sin(phase/2.0);
+	float res;
+	if(std::abs(denom) < DBL_EPSILON) {
+		//This is from Dodge and Jerse (1985), Computer Music: Synthesis, Composition, and Performance. 
+		//It's probably a limit, but it wasn't worth me working through the math to find out.
+		//res = (2*adjusted_harmonics+1)*cos(phase*(adjusted_harmonics+0.5))/cos(phase/2.0);
+		res = adjusted_harmonics+1; //Derived by looking at what 1+2cos(x)+2cos(2x)... does at 0, pi, etc.
+	}
+	else res = (float)(numer/denom);
 	phase += phaseIncrement;
 	//Keep us from going over 2PI. 1 decrement with an if is not sufficient if we're aliasing.
 	phase -= floorf(phase/(2*PI))*2*PI;
-	float res;
-	if(std::abs(denom) < DBL_EPSILON) res = 0.0f; //The numerator goes to 0 here.
-	else res = (float)(numer/denom);
 	return res*normFactor;
 }
 
@@ -61,14 +66,21 @@ inline void Blit::setFrequency(float frequency) {
 
 inline void Blit::recompute() {
 	if(harmonics == 0) {
-		adjusted_harmonics = floor(sr/frequency);
-		if(adjusted_harmonics == 0) adjusted_harmonics = 1;
+		adjusted_harmonics = floor((sr/2)/frequency);
+		//If we divided the sampling rate evenly, we have a harmonic at nyquist.
+		//This would be fine, but the numerator of the above function is then technically aliasing.
+		if(adjusted_harmonics*sr >= sr/2) adjusted_harmonics -= 1;
+		//We have a harmonic at dc, always.  The rest of the harmonics want to cancel in pairs. To that end, we need an odd adjuisted harmonics for the best approximation.
+		if(adjusted_harmonics%2 == 0) adjusted_harmonics -= 1;
+		//But we need at least one.
+		if(adjusted_harmonics <= 0) adjusted_harmonics = 1;
 	}
 	else adjusted_harmonics = harmonics;
 	phaseIncrement = 2*PI*frequency/sr;
 	//1+2cos(x)+2cos(2x)...etc...means max value of 1+2*adjusted_harmonics.
 	if(shouldNormalize) normFactor = 1.0f/(2*adjusted_harmonics+1);
-	else normFactor = 1.0f;
+	//Fourier series of unnormalized BLIT has 1/period=frequency coefficient.
+	else normFactor = frequency;
 }
 
 inline void Blit::setShouldNormalize(bool norm) {
