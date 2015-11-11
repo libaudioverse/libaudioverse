661e5f6fa683e163e39a6d550d7862c94565b21b
Austin Hicks <camlorn38@gmail.com>
2015-11-10 22:49:34 -0500

    Slight optimization to the hRTF implementation.

--- src/libaudioverse/implementations/hrtf_panner.cpp
+++ src/libaudioverse/implementations/hrtf_panner.cpp
@@ -47,14 +47,15 @@ HrtfPanner::~HrtfPanner() {
 void HrtfPanner::pan(float* input, float *left_output, float *right_output) {
 	//Do we need to crossfade? We do if we've moved more than the threshold and crossfading is being allowed.
 	bool needsCrossfade = should_crossfade && fabs(azimuth-prev_azimuth)+fabs(elevation-prev_elevation) >= crossfade_threshold;
-	//We always recompute.
-	std::swap(left_convolver, prev_left_convolver);
-	std::swap(right_convolver, prev_right_convolver);
-	left_convolver->reset();
-	right_convolver->reset();
-	hrtf->computeCoefficientsStereo(elevation, azimuth, left_response, right_response);
-	left_convolver->setResponse(response_length, left_response);
-	right_convolver->setResponse(response_length, right_response);
+	if(azimuth != prev_azimuth || elevation != prev_elevation) {
+		std::swap(left_convolver, prev_left_convolver);
+		std::swap(right_convolver, prev_right_convolver);
+		left_convolver->reset();
+		right_convolver->reset();
+		hrtf->computeCoefficientsStereo(elevation, azimuth, left_response, right_response);
+		left_convolver->setResponse(response_length, left_response);
+		right_convolver->setResponse(response_length, right_response);
+	}
 	//FFT of the input.
 	std::copy(input, input+block_size, fft_workspace);
 	kiss_fftr(fft, fft_workspace, input_fft);
