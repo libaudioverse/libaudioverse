abe50bca3962cc57f73c98130a06897dca1139aa
Austin Hicks <camlorn@camlorn.net>
2016-02-06 13:07:56 -0500

    Initial reimplementation of Multipanner without SubgraphNode.

--- include/libaudioverse/nodes/multipanner.hpp
+++ include/libaudioverse/nodes/multipanner.hpp
@@ -3,6 +3,7 @@ This file is part of Libaudioverse, a library for 3D and environmental audio sim
 A copy of the GPL, as well as other important copyright and licensing information, may be found in the file 'LICENSE' in the root of the Libaudioverse repository.  Should this file be missing or unavailable to you, see <http://www.gnu.org/licenses/>.*/
 #pragma once
 #include "../private/node.hpp"
+#include "../implementations/multipanner.hpp"
 #include <memory>
 
 namespace libaudioverse_implementation {
@@ -10,14 +11,14 @@ namespace libaudioverse_implementation {
 class Simulation;
 class HrtfData;
 
-class MultipannerNode: public SubgraphNode {
+class MultipannerNode: public Node {
 	public:
-	MultipannerNode(std::shared_ptr<Simulation> sim, std::shared_ptr<HrtfData> hrtf);
-	std::shared_ptr<Node> hrtf_panner = nullptr, amplitude_panner = nullptr, input= nullptr, current_panner = nullptr;
-	void configureForwardedProperties();
+	MultipannerNode(std::shared_ptr<Simulation> simulation, std::shared_ptr<HrtfData> hrtf);
 	void strategyChanged();
-	void willTick() override;
+	void process() override;
 	void reset() override;
+	private:
+	Multipanner panner;
 };
 
 std::shared_ptr<Node> createMultipannerNode(std::shared_ptr<Simulation> simulation, std::shared_ptr<HrtfData> hrtf);
