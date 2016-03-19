89ba4a4cd6d3c696f68c3c80d6e69aa48a3aadf5
Austin Hicks <camlorn@camlorn.net>
2016-03-19 17:43:39 -0400

    Disable generation of the zip.

--- appveyor_upload.py
+++ appveyor_upload.py
@@ -4,32 +4,6 @@ import os.path
 import glob
 import subprocess
 
-username = os.getenv("DEPLOYMENT_USERNAME")
-password = os.getenv("DEPLOYMENT_PASSWORD")
-host = os.getenv("DEPLOYMENT_HOST")
-port = 22
-
-if host is None:
-    print("Host?")
-    host = input()
-if username is None:
-    print("Username?")
-    username = input()
-if password is None:
-    print("Password?")
-    password = input()
-
-print("Uploading to camlorn.net.")
-transport=paramiko.Transport(host, 22)
-transport.connect(username = username, password = password)
-with paramiko.SFTPClient.from_transport(transport) as sftp:
-    script_dir = os.path.split(os.path.abspath(__file__))[0]
-    build_dir = os.path.join(script_dir, "build")
-    sftp.chdir("releases/libaudioverse")
-    sftp.put(
-    os.path.join(build_dir, "libaudioverse_master.zip"),
-    "libaudioverse_master.zip")
-    
 print("Attempting to rename wheel for Pypi upload.")
 #We know exactly where the wheel lives.
 path = r"c:\projects\libaudioverse\build\bindings\python\dist\*any.whl"
