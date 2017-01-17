33030ee3ba9e816b8d5df5b02c297c5c159f3094
Austin Hicks <camlorn@camlorn.net>
2017-01-17 18:27:54 -0500

    Rough draft functions for making inputs, outputs, and properties.

--- bindings/bindings_description.py
+++ bindings/bindings_description.py
@@ -1,6 +1,7 @@
 """This file defines the DSL for describing Libaudioverse objects and functions.
 
-You want to see class Builder.  Files in metadata/* are executed with the builder helper functions in the global namespace."""
+You want to see class dsl.Builder.  Files in metadata/* are executed with the builder helper functions in the global namespace."""
+
 import enum
 
 class TypeInfo:
@@ -20,6 +21,8 @@ Whether or not this TypeInfo can represent a typedef depends on context. Objects
 class FunctionCategory:
     """Holds information about a function category.
 
+Function categories primarily exist to differentiate sections in the main manual.  Example: node functions, server functions, etc.
+
 name: The name of the category.
 doc_name: The heading for the category in documentation.
 doc_description: The description of the category in documentation.
@@ -36,15 +39,18 @@ class FunctionInfo:
     """Describes a function.
 
 name: The name of this function.
+doc:  The documentation. None if this function is undocumented or used to get/set a callback.
 return_type: The return type with typedefs translated.
 return_type_pretty: The return type without typedefs translated.
 params: A list of parameters, either SimpleParam or ArrayParam.
 input_params: All parameters which are inputs.
 output_params: All parameters which are outputs.
 category: None, or a FunctionCategory instance. Describes what heading this function belongs under in the language-agnostic manual.
+
+Functions don't have a category if they are for getting/setting callbacks, not documented, or specific to a node.
 """
 
-    def __init__(self, return_type, return_type_pretty, name, params, category):
+    def __init__(self, return_type, return_type_pretty, name, doc, params, category):
         self.return_type = return_type
         self.return_type_pretty = return_type_pretty
         self.name = name
@@ -52,6 +58,7 @@ category: None, or a FunctionCategory instance. Describes what heading this func
         self.input_params = tuple([i for i in params if not i.output])
         self.output_params = tuple([i for i in params if i.output])
         self.category = category
+        self.doc = doc
 
 class SimpleParam:
     """Describes a single parameter, passed as a single value.
@@ -61,15 +68,16 @@ output: true if this parameter is an output parameter, otherwise false.
 type: The type with typedefs translated.
 type_pretty: The type without typedefs translated.
 default: The default value of the param, represented as an appropriate Python object. None if there is no default, and 0 for null pointers.
+doc: The documentation for the parameter. May be None in any case; warnings are printed if this isn't because it's a callback getter/setter or undocumented function.
 """
 
     _uses = 1 # For part of the bindings DSL. How many parameters does this consume?
 
-    def __init__(self, name, type, type_pretty, doc_description, output = None):
+    def __init__(self, name, type, type_pretty, doc, output = None):
         self.type = type
         self.type_pretty = type_pretty
         self.name = name
-        self.doc_description = doc_description
+        self.doc = doc
         if output is None:
             output = "destination" in name.lower()
         self.output = output
@@ -98,18 +106,20 @@ class Enum:
     """Represents a C enum
 
 name: The name.
+doc: The description of the enum if provided, otherwise None.
 members: A  dict mapping member names to EnumMember.
 """
-    def __init__(self, name, members):
+    def __init__(self, name, members, doc):
         self.name = name
         self.members = members
+        self.doc = doc
 
 class EnumMember:
     """Represents an enum member.
 
 name: The name.
 value: The value.
-doc: The description of the member.
+doc: The description of the member, or None if not provided.
 """
 
     def __init__(self, name, value, doc):
@@ -117,15 +127,41 @@ doc: The description of the member.
         self.value = value
         self.doc = doc
 
+ChannelTypes = enum.Enum("ChannelTypes", "constant dynamic constructor")
+
+class ConnectionPoint:
+    """Represents an input to or output from a node.  See the following two subclasses.  All of the following properties are specified:
+
+doc: What the input is for.
+channel_type: Is this input's channel count constant, dynamic, or set by the constructor?
+channels: A number of channels if constant, None if set by the constructor of the node, or a string describing where it comes from.
+"""
+    def __init__(self, doc, channel_type, channels):
+        self.doc = doc
+        self.channel_type = channel_type
+        self.channels = channels
+
+
+class Input(ConnectionPiont):
+    ""Represents an input to a node. See ConnectionPoint docs for details."""
+    pass
+
+class Output(ConnectionPoint):
+    """Represents an output from a node. See ConnectionPoint docs for details."""
+    pass
+
+    """
 class Node:
     """Represents a node.
 
-identifier: The Lav_OBJTYPE_XXX constant as a string.
+identifier: The Lav_OBJTYPE_XXX constant name as a string.
 doc_name: The description for this node, used as the header in the language-agnostic manual.
 doc_description: The long-form description of this node.
 properties: A list of property instances, sorted in alphabetical order by name.
 callbacks: A list of callbacks, sorted in alphabetical order by name.
 extra_functions: A list of extra functions, sorted in alphabetical order by the name of the function representing them in C.
+inputs: A list of input instances in order by index.
+outputs: A list of output instances in order by index.
 """
     def __init__(self, identifier, doc_name, doc_description, properties, callbacks, extra_functions):
         self.identifier = identifier
@@ -136,7 +172,7 @@ extra_functions: A list of extra functions, sorted in alphabetical order by the 
         self.extra_functions = extra_functions
 
 PropertyTypes = enum.Enum("PropertyTypes", "int float double float3 float6 buffer")
-RangeTypes = enum.Enum("RangeTypes", "specified, dynamic, constructor")
+RangeTypes = enum.Enum("RangeTypes", "specified dynamic constructor")
 DefaultTypes = enum.Enum("DefaultTypes", "specified constructor")
 AccessTypes = enum.Enum("AccessTypes", "writable readonly varies")
 
@@ -144,41 +180,52 @@ class Property:
     """Represents a property.
 
 name: The name.
-doc_description: The description.
+doc: The description.  Always present.
+identifier: The name of the C constant representing this property, as a string.
 type: A PropertyTypes.
 range_type: A RangeTypes.
-range: A tuple (min, max) if range is specified. Otherwise a string description.
+range: A tuple (min, max) if range is specified. Otherwise a string description for dynamic ranges or None if the range is set by the constructor.
 default_type: a DefaultTypes.
-default: Either a default value of appropriate type for the property, None if this is a buffer property, or a string description.
+default: Either a default value of appropriate type for the property or None in the case of buffer properties, array properties, and ranges set by constructors.
 access_type: AccessTypes.readonly if the property can only be read, AccessTypes.writable if the property can be written, or AccessTypes.varies if readonly status depends on complex conditions.
 access_type_notes: On properties that use AccessTypes.varies, describes how this varies.
+array_length_range: Either a tuple (min, max), a string describing the range, or None if it's from the constructor. Unspecified value for non-array properties.
+array_length_range_type: A RangeTypes constant, see docs on range and range_type.  Unspecified value for non-array properties.
+
+Note that range tuples may contain infinity, made available in this module as inf. For the array length ranges and any int properties, this means MIN_INT or MAX_INT depending on the sign.
 """
-    def __init__(self, name, doc_description, type, 
+    def __init__(self, name, doc, type, identifier,
         range_type, range, default_type, default,
-        access_type, access_type_notes):
+        access_type, access_type_notes,
+        array_length_range, array_length_range_type):
         self.name = name
-        self.doc_description = doc_description
+        self.doc = doc
         self.type = type
+        self.identifier = identifier
         self.range_type = range_type
         self.range = range
         self.default_type = default_type
         self.default = default
         self.access_type = access_type
         self.access_type_notes = access_type_notes
+        self.array_length_range = array_length_range
+        self.array_length_range_type = array_length_range_type
 
 class Callback:
     """Represents a callback.
 
 name: The name of the callback.
+doc: The callback's documentation.
 getter: The C function which gets this callback.
 setter: The C function which sets this callback.
 in_audio_thread: true if the callback is called in the audio thread.
 type: A FunctionInfo describing the callback's signature.
 """
     def __init__(self, name, getter, setter,
-        in_audio_thread, type):
+        in_audio_thread, type, doc):
         self.name = name
         self.getter = getter
         self.setter = setter
         self.in_audio_thread = in_audio_thread
         self.type = type
+        self.doc = doc
--- bindings/dsl.py
+++ bindings/dsl.py
@@ -1,7 +1,19 @@
 """This is the bindings DSL.
 
-An instance of Builder is created by the bindings generator. Then, all .py files in metadata/* are read into strings and executed with the methods of Builder in the global namespace."""
+An instance of Builder is created by the bindings generator. Then, all .py files in metadata/* are read into strings and executed with the methods of the Builder instance and contents of the bindings_description module in the global namespace.
+
+As a convenience, this module accepts strings anywhere an enum would be needed; in that case, it must be the name of the enum's member."""
 from . import bindings_description as desc
+
+class BuilderError(exception):
+    """An implementation detail. We want something specific to throw."""
+    pass
+
+def conv_enum(enum, name):
+    """Used for enum parameter normalization."""
+    if isinstance(name, str):
+        return getattr(enum, name)
+    return name
 
 class Builder:
     """Builds information for the bindings generator."""
@@ -42,6 +54,8 @@ arrays: A list of tuples containing the names of parameters forming a (length, a
             paramname = i.name
             type_pretty = self._convert_typeinfo(type, translate_typedef = False)
             type = self._convert_typeinfo(type, translate_typedef = True)
+            # This is just boring and straightforward normalization to prevent redundancies in the metadata.
+            # If docs aren't present, we can guess a default. Most functions have a parameter that hits one of these special cases.
             if paramname in param_docs:
                 doc = param_docs[paramname]
             elif paramname == "destination":
@@ -84,3 +98,99 @@ arrays: A list of tuples containing the names of parameters forming a (length, a
             quals.update(td.quals)
             base = td.base
         return desc.TypeInfo(base = base, indirection = indirection, quals = quals)
+
+    def node(self, components, identifier, doc_name, doc_description):
+    """Registers a node.
+
+Most parameters  are self-explanatory.  Components is a list of the return values from the following functions in this file. Do *not* reuse return values from these functions. Ever.
+
+Inputs and outputs count as components. They have inferred indexes based on their order as declared in the components list.
+
+identifier is the Lav_OBJTYPE_xxx constant's name as a string."""
+        # Does identifier exist?
+        if identifier not in self.c_info['constants'] and not identifier.startswith("Lav_OBJTYPE"):
+            raise BuilderError("Node {} with identifier {}: not a valid identifier.".format(name, identifier))
+        properties = [i for i in components if isinstance(i, desc.Property)]
+        properties.sort(key = lambda p: p.name)
+        callbacks = [i for i in components if isinstance(i, desc.Callback)]
+        callbacks.sort(key = lambda c: c.name)
+        extra_functions = [i for i in components if isinstance(i, desc.Function)]
+        extra_functions.sort(key = lambda e: e.name)
+        inputs = [i for i in components if isinstance(i, desc.Input)]
+        outputs = [i for i in components if isinstance(i, desc.Outputs)]
+        node = desc.Node(identifier = identifier, name = name, doc_name = doc_name, doc_description = doc_description,
+            properties = properties, callbacks = callbacks, extra_functions = extra_functions,
+            inputs = inputs, outputs = outputs)
+        #todo: put this somewhere.
+
+    def _connection_builder(self, cls, doc, channel_type, channels):
+        """Builds connections.  User code should not use this function dierctly."""
+        conv_enum(desc.ChannelTypes, channel_type)
+        return cls(doc = doc, channels = channels, channel_type = channel_type)
+
+    def input(self, doc, channel_type, channels):
+        """Builds an input. See docs in bindings_description.ConnectionPoint for parameters."""
+        return self._connection_builder(cls = desc.Input, doc = doc, channel_type = channel_type, channels = channels)
+
+    def output(self, doc, channel_type, channels):
+        """Builds an output. See docs in bindings_description.ConnectionPoint for parameters."""
+        return self._connection_builder(cls = desc.Output, doc = doc, channel_type = channel_type, channels = channels)
+
+    def property(self, name, type, identifier, doc, default = None, default_type = None, range = None, range_type = None, access_type = None, access_type_notes = None):
+        """Builds properties.  Parameters match bindings_description.Property but are validated.
+
+This function offers the following conveniences:
+
+If range is a 2-tuple, range_type is RangeTypes.specified.
+If range is a string, range is RangeTypes.dynamic.
+If range is exactly the string "constructor", range_type is RangeTypes.constructor.
+These conveniences are extended to array length ranges.
+
+If default is None, the default of this property is the appropriate default for the property's type.
+If default is exactly the string "constructor", then default_type is DefaultTypes.constructor.
+Otherwise, the default takes on the value specified.
+Default is ignored for buffer and array properties.
+Defaults for arrays are complicated, and array properties are rare in practice; consequently array property defaults are set by and documented in node constructors.
+
+If left alone, access_type defaults to writable.  If access_type_notes is provided, access_type is ignored and set to AccessTypes.varied.  You should only have to set it to AccessTypes.readonly"""
+        # We normalize, then pass through to the Property class.
+        type = conv_enum(desc.PropertyTypes, type)
+        default_type = conv_enum(desc.DefaultTypes, default_type)
+        range_type = conv_enum(desc.RangeTypes, range_type)
+        array_length_range_type = conv_enum(desc.RangeTypes, array_length_range_type)
+        access_type = conv_enum(desc.AccessTypes, access_type)
+        defaults = {desc.PropertyTypes.int: 0, desc.PropertyTypes.float: 0.0,
+            desc.PropertyTypes.double: 0.0, desc.PropertyTypes.float3: (0.0, 0.0, 0.0),
+            desc.PropertyTypes.float6: (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)}
+        if default == "constructor":
+            default_type = DefaultTypes.constructor
+            default = None
+        elif isinstance(default, str):
+            default_type = desc.DefaultTypes.constructor
+        elif default is None and type in defaults:
+            default = defaults[type]
+        elif type not in {PropertyTypes.buffer, PropertyTypes.int_array, PropertyTypes.float_array}:
+            raise BuilderError("You need to specify a property default.")
+        # Range, range_types.
+        def normalize_range(range, range_type, fail_msg):
+            if isinstance(range, tuple) and len(range) == 2:
+                range_type = RangeTypes.specified
+            elif range == "constructor":
+                range_type = RangeTypes.constructor
+            elif isinstance(range_type, str):
+                range_type = desc.RangeTypes.dynamic
+            elif range is None or range_type is None:
+                raise BuilderError(fail_msg)
+            return (range, range_type)
+        (range, range_type) = normalize_range(range, range_type, "You must specify range and range_type")
+        (array_length_range, array_length_range_type) = normalize_range(array_length_range, array_length_range_type, "You must specify both array_length_range and array_length_range_type")
+        if access_type_notes is not None:
+            access_type = desc.AccessTypes.varied
+        elif access_type is None:
+            access_type = AccessTypes.writable
+        # that's it, finally. So...
+        return desc.Property(name = name, identifier = identifier, doc = doc, type = type,
+            default = default, default_type = default_type,
+            range = range, range_type = range_type,
+            array_length_range = array_length_range, array_length_range_type = array_length_range_type,
+            access_type = access_type, access_type_notes = access_type_notes)
